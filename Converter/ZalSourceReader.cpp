3835fe17225721f41bf7ec1858c29e21b6feec26
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2009-04-08 06:47:51 +0000

    Fix arguments, progress, error checking, etc.

--- Converter/ZalSourceReader.cpp
+++ Converter/ZalSourceReader.cpp
@@ -19,38 +19,62 @@ void CT_ZalSourceReader::FinalRelease()
     CoUninitialize();
 }
 
-HRESULT CT_ZalSourceReader::ProcessSourceFile (BSTR bstr_path)
+HRESULT CT_ZalSourceReader::ProcessSourceFile (BSTR bstr_sourcePath,
+                                               BSTR bstr_dbPath,
+                                               int i_maxEntries)
 {
+    HRESULT h_r = S_OK;
+
     StatusUpdate (0);
 
-    ERROR_LOG (L"GOGO ERRor");
-
-    CString cs_path (bstr_path);
+    CString cs_sourcePath (bstr_sourcePath);
     FILE * io_stream = NULL;
-    errno_t i_error = _tfopen_s (&io_stream, cs_path.GetBuffer(), _T("r, ccs=UNICODE"));
+    errno_t i_error = _tfopen_s (&io_stream, cs_sourcePath.GetBuffer(), L"r, ccs=UNICODE");
     if (0 != i_error)
     {
+        ERROR_LOG (L"Unable to open source file");
         return E_FAIL;
     }
 
     CComPtr<ISqliteWrapper> co_sqlite;
-    HRESULT h_r = co_sqlite.CoCreateInstance (__uuidof(SqliteWrapper));
+    h_r = co_sqlite.CoCreateInstance (__uuidof(SqliteWrapper));
     if (S_OK != h_r)
     {
         return h_r;
     }
 
-    co_sqlite->Open (L"test.db");
-	co_sqlite->Exec (L"CREATE TABLE Test (Entry TEXT)");
+    h_r = co_sqlite->Open (bstr_dbPath);
+    if (S_OK != h_r)
+    {
+        CString cs_msg;
+        cs_msg.Format (L"Unable to open database, error %d");
+        ERROR_LOG ((LPCTSTR)cs_msg);
+        return h_r;
+    }
+
+	h_r = co_sqlite->Exec (L"CREATE TABLE Test (Entry TEXT)");
+    if (S_OK != h_r)
+    {
+        CString cs_msg;
+        cs_msg.Format (L"Unable to create DB table, error %d");
+        ERROR_LOG ((LPCTSTR)cs_msg);
+        return h_r;
+    }
 
     long l_fileLength = _filelength (_fileno (io_stream))/sizeof (TCHAR);
     long l_charsRead = 0;
     int i_percentDone = 0;
+    int i_entriesRead = 0;
 
     TCHAR sz_lineBuf[10000];
     BOOL ui_abort = false;
-    while (!feof (io_stream) && !ui_abort)
+    for (; (!feof (io_stream) && !ui_abort); ++i_entriesRead)
     {
+        if (i_entriesRead == i_maxEntries)
+        {
+            break;
+        }
+
         TCHAR * sz_ret = _fgetts (sz_lineBuf, 10000, io_stream);
         if (NULL == sz_ret)
         {
@@ -63,12 +87,20 @@ HRESULT CT_ZalSourceReader::ProcessSourceFile (BSTR bstr_path)
 
         wstring str_line (sz_lineBuf);
         l_charsRead += str_line.length();
-        int i_pd = (int) (((double)l_charsRead/(double)l_fileLength) * 100);
-        if (i_pd > i_percentDone)
+
+        if (i_maxEntries > 0)
         {
-            i_percentDone = i_pd;
-            StatusUpdate (i_percentDone);
+            i_percentDone = (int) (((double)i_entriesRead/(double)i_maxEntries) * 100);
         }
+        else
+        {
+            int i_pd = (int) (((double)l_charsRead/(double)l_fileLength) * 100);
+            if (i_pd > i_percentDone)
+            {
+                i_percentDone = i_pd;
+            }
+        }
+        StatusUpdate (i_percentDone);
 
         CString cstr_query (L"INSERT INTO Test VALUES('");
         cstr_query += CString (sz_lineBuf);
