8904cc515d8482735610960368daf156e440a67b
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2009-04-08 00:17:56 +0000

    Modify for COM use.

--- Converter/StdAfx.h
+++ Converter/StdAfx.h
@@ -1,37 +1,34 @@
 // stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
+// or project specific include files that are used frequently,
+// but are changed infrequently
 
 #pragma once
 
-#ifndef VC_EXTRALEAN
-#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+#ifndef STRICT
+#define STRICT
 #endif
 
-// Modify the following defines if you have to target a platform prior to the ones specified below.
-// Refer to MSDN for the latest info on corresponding values for different platforms.
-#ifndef WINVER				// Allow use of features specific to Windows XP or later.
-#define WINVER 0x0501		// Change this to the appropriate value to target other versions of Windows.
-#endif
+#include "targetver.h"
 
-#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
-#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
-#endif						
-
-#ifndef _WIN32_WINDOWS		// Allow use of features specific to Windows 98 or later.
-#define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
-#endif
-
-#ifndef _WIN32_IE			// Allow use of features specific to IE 6.0 or later.
-#define _WIN32_IE 0x0600	// Change this to the appropriate value to target other versions of IE.
-#endif
+#define _ATL_APARTMENT_THREADED
+#define _ATL_NO_AUTOMATIC_NAMESPACE
 
 #define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	// some CString constructors will be explicit
 
-#include <windows.h>
+#include "resource.h"
 
 //
-// STL stuff
+// ATL
+//
+#include <atlbase.h>
+#include <atlcom.h>
+#include <atlctl.h>
+#include <atlstr.h>
+
+using namespace ATL;
+
+//
+// STL
 //
 #include <vector>
 #include <string>
@@ -44,21 +41,20 @@
 #include <ctime>
 #include <tchar.h>
 
-#include "ExtString.h"
-#include "Error.h"
+//
+//
+// CRT
+#include <io.h>
 
-//std::wstring str_Location()
-//{
-//    return std::wstring (_T(__FILE__)) + 
-//           str_ToString (__LINE__) + 
-//           std::wstring (_T(__FUNCTION__));
-//}
+#include "Error.h"
 
 #define ERROR_LOG(str_Msg) wstringstream io_; \
         io_ << __LINE__; \
-        Error (str_Msg, \
+        CT_Error (str_Msg, \
            std::wstring (_T(__FILE__)) + \
            std::wstring (_T("\t")) + \
            io_.str() + \
            std::wstring (_T("\t")) + \
            std::wstring (_T(__FUNCTION__)));
+
+
