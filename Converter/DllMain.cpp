ff38c645591235c6c293203aa02580fee968b539
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2009-04-08 00:12:21 +0000

    Initial revision.

---
+++ Converter/DllMain.cpp
@@ -0,0 +1,15 @@
+// dllmain.cpp : Implementation of DllMain.
+
+#include "stdafx.h"
+#include "resource.h"
+#include "ConversionLib_i.h"
+#include "dllmain.h"
+
+CConversionLibModule _AtlModule;
+
+// DLL Entry Point
+extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+	hInstance;
+	return _AtlModule.DllMain(dwReason, lpReserved); 
+}
---
+++ Converter/DllMain.h
@@ -0,0 +1,10 @@
+// dllmain.h : Declaration of module class.
+
+class CConversionLibModule : public CAtlDllModuleT< CConversionLibModule >
+{
+public :
+	DECLARE_LIBID(LIBID_ZalConversionLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_CONVERSIONLIB, "{97F389FC-5341-49DE-BFBD-E4C9E174F501}")
+};
+
+extern class CConversionLibModule _AtlModule;
