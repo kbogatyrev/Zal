5fd971a0c8dbbdf1abb183a01dd600401108ba7a
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2009-10-02 16:13:48 +0000

    Add headword and stress data to lexeme.

--- MainLib/Dictionary.cpp
+++ MainLib/Dictionary.cpp
@@ -12,16 +12,59 @@ wstring static str_ToString (T from)
 	return io_.str();
 };
 
+static wstring str_queryBase (L"select headword.source, \
+                                stress.position, \
+                                stress.is_primary, \
+                                descriptor.id, \
+                                descriptor.graphic_stem, \
+                                descriptor.is_variant, \
+                                descriptor.main_symbol, \
+                                descriptor.is_plural_of, \
+                                descriptor.is_intransitive, \
+                                descriptor.main_symbol_plural_of, \
+                                descriptor.alt_main_symbol, \
+                                descriptor.inflection_type, \
+                                descriptor.part_of_speech, \
+                                descriptor.comment, \
+                                descriptor.alt_main_symbol_comment, \
+                                descriptor.alt_inflection_comment, \
+                                descriptor.verb_stem_alternation,  \
+                                descriptor.section, \
+                                descriptor.no_comparative, \
+                                descriptor.assumed_forms, \
+                                descriptor.yo_alternation, \
+                                descriptor.o_alternation, \
+                                descriptor.second_genitive, \
+                                descriptor.questionable_forms, \
+                                descriptor.irregular_forms, \
+                                descriptor.restricted_forms, \
+                                descriptor.contexts, \
+                                descriptor.trailing_comment, \
+                                inflection.is_primary, \
+                                inflection.inflection_type, \
+                                inflection.accent_type1, \
+                                inflection.accent_type2, \
+                                inflection.short_form_restrictions, \
+                                inflection.past_part_restrictions, \
+                                inflection.no_short_form, \
+                                inflection.no_past_part, \
+                                inflection.fleeting_vowel, \
+                                inflection.stem_augment, \
+                                inflection.common_deviation \
+                            from \
+                                headword left outer join stress on stress.headword_id = headword.id \
+                                inner join descriptor on descriptor.word_id = headword.id \
+                                left outer join inflection on descriptor.id = inflection.descriptor_id ");
+
 HRESULT CT_Dictionary::get_Lexeme (long lId, ILexeme ** p_lexeme)
 {
     HRESULT h_r = S_OK;
 
     *p_lexeme = NULL;
 
-    wstring str_query (L"select from descriptor, left outer join inflection \
-                        where descriptor.id = inflection.descriptor_id and descriptor.id = ");
+    wstring str_query (str_queryBase);
+    str_query += L"where descriptor.id = ";
     str_query += str_ToString (lId);
-
     h_r = h_GetData (str_query);
 
     return h_r;
@@ -33,11 +76,10 @@ HRESULT CT_Dictionary::get_Lexemes (BSTR bstr_stem)//, IZalCollection ** pi_lexe
 
     HRESULT h_r = S_OK;
 
-    wstring str_query (L"select distinct * from descriptor left outer join inflection \
-                        where descriptor.id = inflection.descriptor_id and descriptor.graphic_stem = \"");
+    wstring str_query (str_queryBase);
+    str_query += L"where descriptor.graphic_stem = \"";
     str_query += OLE2W (bstr_stem);
     str_query += '\"';
-
     h_r = h_GetData (str_query);
 
     return h_r;
@@ -64,31 +106,45 @@ HRESULT CT_Dictionary::h_GetData (const wstring& str_select)
                 return h_r;
             }
     
-            pco_Db->v_GetData (0, sp_lexeme->i_DbKey);         // 0-based
-            pco_Db->v_GetData (2, sp_lexeme->str_GraphicStem);
-            pco_Db->v_GetData (3, sp_lexeme->b_IsVariant);
-            pco_Db->v_GetData (4, sp_lexeme->str_MainSymbol);
-            pco_Db->v_GetData (5, sp_lexeme->b_IsPluralOf);
-            pco_Db->v_GetData (6, sp_lexeme->b_Transitive);
-            pco_Db->v_GetData (7, sp_lexeme->str_MainSymbolPluralOf);
-            pco_Db->v_GetData (8, sp_lexeme->str_AltMainSymbol);
-            pco_Db->v_GetData (9, sp_lexeme->str_InflectionType);
-            pco_Db->v_GetData (10, (int&)sp_lexeme->eo_PartOfSpeech);
-            pco_Db->v_GetData (11, sp_lexeme->str_Comment);
-            pco_Db->v_GetData (12, sp_lexeme->str_AltMainSymbolComment);
-            pco_Db->v_GetData (13, sp_lexeme->str_AltInflectionComment);
-            pco_Db->v_GetData (14, sp_lexeme->str_VerbStemAlternation);
-            pco_Db->v_GetData (15, sp_lexeme->i_Section);
-            pco_Db->v_GetData (16, sp_lexeme->b_NoComparative);
-            pco_Db->v_GetData (17, sp_lexeme->b_AssumedForms);
-            pco_Db->v_GetData (18, sp_lexeme->b_YoAlternation);
-            pco_Db->v_GetData (19, sp_lexeme->b_OAlternation);
-            pco_Db->v_GetData (20, sp_lexeme->b_SecondGenitive);
-            pco_Db->v_GetData (21, sp_lexeme->str_QuestionableForms);
-            pco_Db->v_GetData (22, sp_lexeme->str_IrregularForms);
-            pco_Db->v_GetData (23, sp_lexeme->str_RestrictedFroms);
-            pco_Db->v_GetData (24, sp_lexeme->str_Contexts);
-            pco_Db->v_GetData (25, sp_lexeme->str_TrailingComment);
+            int i_stressPos = -1;
+            bool b_isStressPrimary = false;
+
+            pco_Db->v_GetData (0, sp_lexeme->str_SourceForm);
+            pco_Db->v_GetData (1, i_stressPos);
+            pco_Db->v_GetData (2, b_isStressPrimary);
+            if (b_isStressPrimary)
+            {
+                sp_lexeme->i_SourceStressPos = i_stress;
+            }
+            else
+            {
+                sp_lexeme->i_SecondarySourceStressPos = i_stress;
+            }
+            pco_Db->v_GetData (3, sp_lexeme->i_DbKey);
+            pco_Db->v_GetData (4, sp_lexeme->str_GraphicStem);
+            pco_Db->v_GetData (5, sp_lexeme->b_IsVariant);
+            pco_Db->v_GetData (6, sp_lexeme->str_MainSymbol);
+            pco_Db->v_GetData (7, sp_lexeme->b_IsPluralOf);
+            pco_Db->v_GetData (8, sp_lexeme->b_Transitive);
+            pco_Db->v_GetData (9, sp_lexeme->str_MainSymbolPluralOf);
+            pco_Db->v_GetData (10, sp_lexeme->str_AltMainSymbol);
+            pco_Db->v_GetData (11, sp_lexeme->str_InflectionType);
+            pco_Db->v_GetData (12, (int&)sp_lexeme->eo_PartOfSpeech);
+            pco_Db->v_GetData (13, sp_lexeme->str_Comment);
+            pco_Db->v_GetData (14, sp_lexeme->str_AltMainSymbolComment);
+            pco_Db->v_GetData (15, sp_lexeme->str_AltInflectionComment);
+            pco_Db->v_GetData (16, sp_lexeme->str_VerbStemAlternation);
+            pco_Db->v_GetData (17, sp_lexeme->i_Section);
+            pco_Db->v_GetData (18, sp_lexeme->b_NoComparative);
+            pco_Db->v_GetData (19, sp_lexeme->b_AssumedForms);
+            pco_Db->v_GetData (20, sp_lexeme->b_YoAlternation);
+            pco_Db->v_GetData (21, sp_lexeme->b_OAlternation);
+            pco_Db->v_GetData (22, sp_lexeme->b_SecondGenitive);
+            pco_Db->v_GetData (23, sp_lexeme->str_QuestionableForms);
+            pco_Db->v_GetData (24, sp_lexeme->str_IrregularForms);
+            pco_Db->v_GetData (25, sp_lexeme->str_RestrictedFroms);
+            pco_Db->v_GetData (26, sp_lexeme->str_Contexts);
+            pco_Db->v_GetData (27, sp_lexeme->str_TrailingComment);
             pco_Db->v_GetData (28, sp_lexeme->b_PrimaryInflectionGroup);
             pco_Db->v_GetData (29, sp_lexeme->i_Type);
             pco_Db->v_GetData (30, (int&)sp_lexeme->eo_AccentType1);
