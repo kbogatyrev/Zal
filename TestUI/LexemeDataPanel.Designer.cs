1654c8246f2010afe21425f07c739d6c635f2b7a
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2009-10-27 20:09:33 +0000

    Initial revision.

---
+++ TestUI/LexemeDataPanel.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace TestUI
+{
+    partial class LexemeDataPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInitForm = new System.Windows.Forms.TextBox();
+            this.textBoxInitFormValue = new System.Windows.Forms.TextBox();
+            this.textBoxGrStem = new System.Windows.Forms.TextBox();
+            this.textBoxGrStemValue = new System.Windows.Forms.TextBox();
+            this.textBoxMainSymb = new System.Windows.Forms.TextBox();
+            this.textBoxMainSymbValue = new System.Windows.Forms.TextBox();
+            this.textBoxType = new System.Windows.Forms.TextBox();
+            this.textBoxTypeValue = new System.Windows.Forms.TextBox();
+            this.textBoxStress = new System.Windows.Forms.TextBox();
+            this.textBoxStressTypeValue = new System.Windows.Forms.TextBox();
+            this.textBoxAdvanced = new System.Windows.Forms.TextBox();
+            this.textBoxAdvancedValue = new System.Windows.Forms.TextBox();
+            this.buttonShowWF = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxInitForm
+            // 
+            this.textBoxInitForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInitForm.Location = new System.Drawing.Point(18, 9);
+            this.textBoxInitForm.Name = "textBoxInitForm";
+            this.textBoxInitForm.ReadOnly = true;
+            this.textBoxInitForm.Size = new System.Drawing.Size(100, 20);
+            this.textBoxInitForm.TabIndex = 0;
+            this.textBoxInitForm.Text = "Initial Form";
+            // 
+            // textBoxInitFormValue
+            // 
+            this.textBoxInitFormValue.Location = new System.Drawing.Point(124, 9);
+            this.textBoxInitFormValue.Name = "textBoxInitFormValue";
+            this.textBoxInitFormValue.Size = new System.Drawing.Size(185, 20);
+            this.textBoxInitFormValue.TabIndex = 1;
+            // 
+            // textBoxGrStem
+            // 
+            this.textBoxGrStem.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxGrStem.Location = new System.Drawing.Point(18, 36);
+            this.textBoxGrStem.Name = "textBoxGrStem";
+            this.textBoxGrStem.ReadOnly = true;
+            this.textBoxGrStem.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGrStem.TabIndex = 2;
+            this.textBoxGrStem.Text = "Graphic Stem";
+            // 
+            // textBoxGrStemValue
+            // 
+            this.textBoxGrStemValue.Location = new System.Drawing.Point(124, 36);
+            this.textBoxGrStemValue.Name = "textBoxGrStemValue";
+            this.textBoxGrStemValue.Size = new System.Drawing.Size(185, 20);
+            this.textBoxGrStemValue.TabIndex = 3;
+            // 
+            // textBoxMainSymb
+            // 
+            this.textBoxMainSymb.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxMainSymb.Location = new System.Drawing.Point(18, 63);
+            this.textBoxMainSymb.Name = "textBoxMainSymb";
+            this.textBoxMainSymb.ReadOnly = true;
+            this.textBoxMainSymb.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMainSymb.TabIndex = 4;
+            this.textBoxMainSymb.Text = "Main Symbol";
+            // 
+            // textBoxMainSymbValue
+            // 
+            this.textBoxMainSymbValue.Location = new System.Drawing.Point(124, 63);
+            this.textBoxMainSymbValue.Name = "textBoxMainSymbValue";
+            this.textBoxMainSymbValue.Size = new System.Drawing.Size(185, 20);
+            this.textBoxMainSymbValue.TabIndex = 5;
+            // 
+            // textBoxType
+            // 
+            this.textBoxType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxType.Location = new System.Drawing.Point(18, 90);
+            this.textBoxType.Name = "textBoxType";
+            this.textBoxType.ReadOnly = true;
+            this.textBoxType.Size = new System.Drawing.Size(100, 20);
+            this.textBoxType.TabIndex = 6;
+            this.textBoxType.Text = "Type";
+            // 
+            // textBoxTypeValue
+            // 
+            this.textBoxTypeValue.Location = new System.Drawing.Point(125, 90);
+            this.textBoxTypeValue.Name = "textBoxTypeValue";
+            this.textBoxTypeValue.Size = new System.Drawing.Size(184, 20);
+            this.textBoxTypeValue.TabIndex = 7;
+            // 
+            // textBoxStress
+            // 
+            this.textBoxStress.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxStress.Location = new System.Drawing.Point(18, 116);
+            this.textBoxStress.Name = "textBoxStress";
+            this.textBoxStress.ReadOnly = true;
+            this.textBoxStress.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStress.TabIndex = 8;
+            this.textBoxStress.Text = "Stress Type";
+            // 
+            // textBoxStressTypeValue
+            // 
+            this.textBoxStressTypeValue.Location = new System.Drawing.Point(125, 117);
+            this.textBoxStressTypeValue.Name = "textBoxStressTypeValue";
+            this.textBoxStressTypeValue.Size = new System.Drawing.Size(184, 20);
+            this.textBoxStressTypeValue.TabIndex = 9;
+            // 
+            // textBoxAdvanced
+            // 
+            this.textBoxAdvanced.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAdvanced.Location = new System.Drawing.Point(18, 143);
+            this.textBoxAdvanced.Name = "textBoxAdvanced";
+            this.textBoxAdvanced.ReadOnly = true;
+            this.textBoxAdvanced.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAdvanced.TabIndex = 10;
+            this.textBoxAdvanced.Text = "Advanced";
+            // 
+            // textBoxAdvancedValue
+            // 
+            this.textBoxAdvancedValue.Location = new System.Drawing.Point(125, 142);
+            this.textBoxAdvancedValue.Multiline = true;
+            this.textBoxAdvancedValue.Name = "textBoxAdvancedValue";
+            this.textBoxAdvancedValue.Size = new System.Drawing.Size(184, 49);
+            this.textBoxAdvancedValue.TabIndex = 11;
+            // 
+            // buttonShowWF
+            // 
+            this.buttonShowWF.Location = new System.Drawing.Point(324, 87);
+            this.buttonShowWF.Name = "buttonShowWF";
+            this.buttonShowWF.Size = new System.Drawing.Size(75, 23);
+            this.buttonShowWF.TabIndex = 12;
+            this.buttonShowWF.Text = "Show Forms";
+            this.buttonShowWF.UseVisualStyleBackColor = true;
+            this.buttonShowWF.Click += new System.EventHandler(this.buttonShowWF_Click);
+            // 
+            // LexemeDataPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.buttonShowWF);
+            this.Controls.Add(this.textBoxAdvancedValue);
+            this.Controls.Add(this.textBoxAdvanced);
+            this.Controls.Add(this.textBoxStressTypeValue);
+            this.Controls.Add(this.textBoxStress);
+            this.Controls.Add(this.textBoxTypeValue);
+            this.Controls.Add(this.textBoxType);
+            this.Controls.Add(this.textBoxMainSymbValue);
+            this.Controls.Add(this.textBoxMainSymb);
+            this.Controls.Add(this.textBoxGrStemValue);
+            this.Controls.Add(this.textBoxGrStem);
+            this.Controls.Add(this.textBoxInitFormValue);
+            this.Controls.Add(this.textBoxInitForm);
+            this.Name = "LexemeDataPanel";
+            this.Size = new System.Drawing.Size(419, 199);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInitForm;
+        private System.Windows.Forms.TextBox textBoxInitFormValue;
+        private System.Windows.Forms.TextBox textBoxGrStem;
+        private System.Windows.Forms.TextBox textBoxGrStemValue;
+        private System.Windows.Forms.TextBox textBoxMainSymb;
+        private System.Windows.Forms.TextBox textBoxMainSymbValue;
+        private System.Windows.Forms.TextBox textBoxType;
+        private System.Windows.Forms.TextBox textBoxTypeValue;
+        private System.Windows.Forms.TextBox textBoxStress;
+        private System.Windows.Forms.TextBox textBoxStressTypeValue;
+        private System.Windows.Forms.TextBox textBoxAdvanced;
+        private System.Windows.Forms.TextBox textBoxAdvancedValue;
+        private System.Windows.Forms.Button buttonShowWF;
+    }
+}
---
+++ TestUI/LexemeDataPanel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestUI
+{
+//    enum etInputDataType { eGraphicStem, eInitialForm };
+
+    public partial class LexemeDataPanel : UserControl
+    {
+        public delegate void ShowDetails (int iLexemeId);
+        public event ShowDetails ShowDetailsEvent;
+
+        int iLexemeId;
+
+        public string sInitialForm
+        {
+            get
+            {
+                return textBoxInitFormValue.Text;
+            }
+
+            set
+            {
+                textBoxInitFormValue.Text = value;
+            }
+        }
+
+        public string sGraphicStem
+        {
+            get
+            {
+                return textBoxGrStemValue.Text;
+            }
+
+            set
+            {
+                textBoxGrStemValue.Text = value;
+            }
+        }
+
+        public string sMainSymbol
+        {
+            get
+            {
+                return textBoxMainSymbValue.Text;
+            }
+
+            set
+            {
+                textBoxMainSymbValue.Text = value;
+            }
+        }
+
+        public string sType
+        {
+            get
+            {
+                return textBoxTypeValue.Text;
+            }
+
+            set
+            {
+                textBoxTypeValue.Text = value;
+            }
+        }
+
+        public string sStressType
+        {
+            get
+            {
+                return textBoxStressTypeValue.Text;
+            }
+
+            set
+            {
+                textBoxStressTypeValue.Text = value;
+            }
+        }
+
+        public string Advanced
+        {
+            get
+            {
+                return textBoxAdvancedValue.Text;
+            }
+
+            set
+            {
+                textBoxAdvancedValue.Text = value;
+            }
+        }
+
+        public LexemeDataPanel (int iId)
+        {
+            InitializeComponent();
+            iLexemeId = iId;
+        }
+
+        private void buttonShowWF_Click (object sender, EventArgs e)
+        {
+            if (ShowDetailsEvent != null)
+            {
+                ShowDetailsEvent(iLexemeId);
+            }
+        }
+    }
+}
