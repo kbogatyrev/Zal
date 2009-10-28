10a6d457b15a6047f29c8a06950d3efed6d40ff3
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2009-10-28 16:58:21 +0000

    Change name of the form.

--- TestUI/Program.cs
+++ TestUI/Program.cs
@@ -15,7 +15,7 @@ namespace TestUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Synthesizer());
+            Application.Run(new TestApplet());
         }
     }
 }
