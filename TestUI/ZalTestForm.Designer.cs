32b32cb4a06f06d3e80759c8711fb9708eb5fca5
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2009-10-31 23:14:26 +0000

    Resizing, scroll bars etc.

--- TestUI/ZalTestForm.Designer.cs
+++ TestUI/ZalTestForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.menuFile = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pathToDBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tabSearch = new System.Windows.Forms.TabPage();
             this.panelLex = new System.Windows.Forms.Panel();
             this.radioButtonWordForm = new System.Windows.Forms.RadioButton();
@@ -41,7 +42,6 @@
             this.textBoxSearchString = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.tabControl = new System.Windows.Forms.TabControl();
-            this.pathToDBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuFile.SuspendLayout();
             this.tabSearch.SuspendLayout();
             this.panelLex.SuspendLayout();
@@ -54,7 +54,7 @@
             this.fileToolStripMenuItem});
             this.menuFile.Location = new System.Drawing.Point(0, 0);
             this.menuFile.Name = "menuFile";
-            this.menuFile.Size = new System.Drawing.Size(648, 24);
+            this.menuFile.Size = new System.Drawing.Size(702, 24);
             this.menuFile.TabIndex = 1;
             this.menuFile.Text = "File";
             // 
@@ -66,13 +66,20 @@
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
+            // pathToDBToolStripMenuItem
+            // 
+            this.pathToDBToolStripMenuItem.Name = "pathToDBToolStripMenuItem";
+            this.pathToDBToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.pathToDBToolStripMenuItem.Text = "Path to DB";
+            this.pathToDBToolStripMenuItem.Click += new System.EventHandler(this.pathToDBToolStripMenuItem_Click);
+            // 
             // tabSearch
             // 
             this.tabSearch.Controls.Add(this.panelLex);
             this.tabSearch.Location = new System.Drawing.Point(4, 22);
             this.tabSearch.Name = "tabSearch";
             this.tabSearch.Padding = new System.Windows.Forms.Padding(3);
-            this.tabSearch.Size = new System.Drawing.Size(640, 678);
+            this.tabSearch.Size = new System.Drawing.Size(694, 791);
             this.tabSearch.TabIndex = 0;
             this.tabSearch.Text = "Search";
             this.tabSearch.UseVisualStyleBackColor = true;
@@ -87,16 +94,15 @@
             this.panelLex.Controls.Add(this.radioButtonGStem);
             this.panelLex.Controls.Add(this.textBoxSearchString);
             this.panelLex.Controls.Add(this.textBox1);
-            this.panelLex.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelLex.Location = new System.Drawing.Point(3, 3);
+            this.panelLex.Location = new System.Drawing.Point(3, -44);
             this.panelLex.Name = "panelLex";
-            this.panelLex.Size = new System.Drawing.Size(634, 672);
+            this.panelLex.Size = new System.Drawing.Size(688, 827);
             this.panelLex.TabIndex = 0;
             // 
             // radioButtonWordForm
             // 
             this.radioButtonWordForm.AutoSize = true;
-            this.radioButtonWordForm.Location = new System.Drawing.Point(386, 33);
+            this.radioButtonWordForm.Location = new System.Drawing.Point(445, 61);
             this.radioButtonWordForm.Name = "radioButtonWordForm";
             this.radioButtonWordForm.Size = new System.Drawing.Size(91, 17);
             this.radioButtonWordForm.TabIndex = 7;
@@ -108,14 +114,14 @@
             // 
             this.lexPanel.AutoScroll = true;
             this.lexPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lexPanel.Location = new System.Drawing.Point(23, 112);
+            this.lexPanel.Location = new System.Drawing.Point(23, 131);
             this.lexPanel.Name = "lexPanel";
-            this.lexPanel.Size = new System.Drawing.Size(587, 533);
+            this.lexPanel.Size = new System.Drawing.Size(639, 680);
             this.lexPanel.TabIndex = 6;
             // 
             // buttonLookup
             // 
-            this.buttonLookup.Location = new System.Drawing.Point(535, 50);
+            this.buttonLookup.Location = new System.Drawing.Point(559, 76);
             this.buttonLookup.Name = "buttonLookup";
             this.buttonLookup.Size = new System.Drawing.Size(75, 23);
             this.buttonLookup.TabIndex = 5;
@@ -126,7 +132,7 @@
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(73, 32);
+            this.textBox3.Location = new System.Drawing.Point(73, 61);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
             this.textBox3.Size = new System.Drawing.Size(74, 20);
@@ -136,7 +142,7 @@
             // radioButtonInitForm
             // 
             this.radioButtonInitForm.AutoSize = true;
-            this.radioButtonInitForm.Location = new System.Drawing.Point(287, 32);
+            this.radioButtonInitForm.Location = new System.Drawing.Point(315, 61);
             this.radioButtonInitForm.Name = "radioButtonInitForm";
             this.radioButtonInitForm.Size = new System.Drawing.Size(86, 17);
             this.radioButtonInitForm.TabIndex = 3;
@@ -147,7 +153,7 @@
             // radioButtonGStem
             // 
             this.radioButtonGStem.AutoSize = true;
-            this.radioButtonGStem.Location = new System.Drawing.Point(174, 32);
+            this.radioButtonGStem.Location = new System.Drawing.Point(174, 61);
             this.radioButtonGStem.Name = "radioButtonGStem";
             this.radioButtonGStem.Size = new System.Drawing.Size(89, 17);
             this.radioButtonGStem.TabIndex = 2;
@@ -157,16 +163,16 @@
             // 
             // textBoxSearchString
             // 
-            this.textBoxSearchString.Location = new System.Drawing.Point(174, 66);
+            this.textBoxSearchString.Location = new System.Drawing.Point(174, 95);
             this.textBoxSearchString.Name = "textBoxSearchString";
-            this.textBoxSearchString.Size = new System.Drawing.Size(298, 20);
+            this.textBoxSearchString.Size = new System.Drawing.Size(362, 20);
             this.textBoxSearchString.TabIndex = 1;
             this.textBoxSearchString.TextChanged += new System.EventHandler(this.textBoxSearchString_TextChanged);
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(23, 66);
+            this.textBox1.Location = new System.Drawing.Point(23, 95);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(124, 20);
@@ -180,21 +186,14 @@
             this.tabControl.Location = new System.Drawing.Point(0, 27);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(648, 704);
+            this.tabControl.Size = new System.Drawing.Size(702, 817);
             this.tabControl.TabIndex = 0;
-            // 
-            // pathToDBToolStripMenuItem
-            // 
-            this.pathToDBToolStripMenuItem.Name = "pathToDBToolStripMenuItem";
-            this.pathToDBToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.pathToDBToolStripMenuItem.Text = "Path to DB";
-            this.pathToDBToolStripMenuItem.Click += new System.EventHandler(this.pathToDBToolStripMenuItem_Click);
             // 
             // TestApplet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(648, 731);
+            this.ClientSize = new System.Drawing.Size(702, 844);
             this.Controls.Add(this.menuFile);
             this.Controls.Add(this.tabControl);
             this.Name = "TestApplet";
