5b29689bf0055515a05c4e6ee4592381be89fb4e
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2009-11-01 07:49:08 +0000

    Move reusable code to base class.

--- TestUI/AdjPanel.cs
+++ TestUI/AdjPanel.cs
@@ -9,22 +9,8 @@ using System.Windows.Forms;
 
 namespace TestUI
 {
-    public partial class AdjPanel : UserControl
+    public partial class AdjPanel : WordFormsPanel
     {
-        public struct StCell
-        {
-            public TextBox textboxTarget;
-            public HashSet<string> setContents;
-
-            public StCell (TextBox textBox)
-            {
-                textboxTarget = textBox;
-                setContents = new HashSet<string>();
-            }
-        }
-
-        Dictionary<string, StCell> m_dictForms;
-
         public string sLexName
         {
             get
@@ -37,40 +23,6 @@ namespace TestUI
                 textBoxLexName.Text = value;
             }
         }
-
-        public void Clear()
-        {
-            foreach (KeyValuePair <string, StCell> entry in m_dictForms)
-            {
-                entry.Value.textboxTarget.Text.Remove(0);
-            }
-        }
-
-        public void SetForm (string sKey, string sText)
-        {
-            if (m_dictForms[sKey].setContents.Contains (sText))
-            {
-                return;
-            }
-
-            m_dictForms[sKey].setContents.Add (sText);
-
-            if (m_dictForms[sKey].textboxTarget.Text.Length > 0)
-            {
-                m_dictForms[sKey].textboxTarget.Text += "\r\n";
-            }
-            m_dictForms[sKey].textboxTarget.Text += sText;
-
-            if (m_dictForms[sKey].textboxTarget.Lines.Length > 1)
-            {
-                m_dictForms[sKey].textboxTarget.ScrollBars = ScrollBars.Vertical;
-            }
-            else
-            {
-                m_dictForms[sKey].textboxTarget.ScrollBars = ScrollBars.None;
-            }
-        }
-
         public AdjPanel()
         {
             InitializeComponent();
