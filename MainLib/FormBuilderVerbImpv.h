d625f36d10bbfe5ca66a63065a116074cd9b06b6
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2010-03-18 17:09:17 +0000

    Refactoring.

--- MainLib/FormBuilderVerbImpv.h
+++ MainLib/FormBuilderVerbImpv.h
@@ -13,10 +13,14 @@ public:
     {}
 
 public:
-    HRESULT h_GetLemma (CT_ExtString& xstr_lemma);
-    HRESULT h_GetEnding (CT_ExtString& xstr_lemma, wstring& str_ending);
-    HRESULT h_GetStressPositions (CT_ExtString& xstr_lemma, 
+    HRESULT h_GetLemma (wstring& str_lemma);
+    HRESULT h_GetEnding (const wstring& str_lemma, wstring& str_ending);
+    HRESULT h_GetStressPositions (wstring& str_lemma, 
                                   wstring& str_ending,
                                   vector<int>& vec_stressPositions);
+    HRESULT h_CreateFormTemplate (const wstring& str_lemma,
+                                  wstring& str_ending,
+                                  ET_Number eo_number,
+                                  CComObject<CT_WordForm> *& pco_wordForm);
     HRESULT h_Build();
 };
