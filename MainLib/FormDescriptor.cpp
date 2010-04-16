77f63e70402ecd4b2dc31eccec7dfee0f2a6ce97
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2010-04-16 16:37:35 +0000

    Initial revision.

---
+++ MainLib/FormDescriptor.cpp
@@ -0,0 +1,117 @@
+#include "StdAfx.h"
+#include "Lexeme.h"
+#include "FormDescriptor.h"
+
+//
+// IFormDescriptor
+//
+
+HRESULT CT_FormDescriptor::FindForms()
+{
+    try
+    {
+        v_FormsFromHash();
+    }
+    catch (CT_Exception co_ex)
+    {
+        return E_FAIL;  // logging should be always done by callee
+    }
+
+    return S_OK;
+}
+
+HRESULT CT_FormDescriptor::put_PartOfSpeech (ET_PartOfSpeech eo_pos)
+{
+    co_Hasher.eo_POS = eo_pos;
+    return S_OK;
+}
+
+HRESULT CT_FormDescriptor::put_Subparadigm (ET_Subparadigm eo_sp)
+{
+    co_Hasher.eo_Subparadigm = eo_sp;
+    return S_OK;
+}
+
+HRESULT CT_FormDescriptor::put_Case (ET_Case eo_case)
+{
+    co_Hasher.eo_Case = eo_case;
+    return S_OK;
+}
+
+HRESULT CT_FormDescriptor::put_Number (ET_Number eo_number)
+{
+    co_Hasher.eo_Number = eo_number;
+    return S_OK;
+}
+
+HRESULT CT_FormDescriptor::put_Gender (ET_Gender eo_gender)
+{
+    co_Hasher.eo_Gender = eo_gender;
+    return S_OK;
+}
+
+HRESULT CT_FormDescriptor::put_Tense (ET_Tense eo_tense)
+{
+    co_Hasher.eo_Tense = eo_tense;
+    return S_OK;
+}
+
+HRESULT CT_FormDescriptor::put_Person (ET_Person eo_person)
+{
+    co_Hasher.eo_Person = eo_person;
+    return S_OK;
+}
+
+HRESULT CT_FormDescriptor::put_Animacy (ET_Animacy eo_animacy)
+{
+    co_Hasher.eo_Animacy = eo_animacy;
+    return S_OK;
+}
+
+HRESULT CT_FormDescriptor::put_Reflexivity (ET_Reflexive eo_reflexivity)
+{
+    co_Hasher.eo_Reflexive = eo_reflexivity;
+    return S_OK;
+}
+
+HRESULT CT_FormDescriptor::put_Voice (ET_Voice eo_voice)
+{
+    co_Hasher.eo_Voice = eo_voice;
+    return S_OK;
+}
+
+HRESULT CT_FormDescriptor::put_Aspect (ET_Aspect eo_aspect)
+{
+    co_Hasher.eo_Aspect = eo_aspect;
+    return S_OK;
+}
+
+void CT_FormDescriptor::v_FormsFromHash()
+{
+    pair<multimap<int, CComVariant>::iterator, multimap<int, CComVariant>::iterator> pair_range;
+    pair_range = sp_Lexeme->m_coll.equal_range (co_Hasher.i_GramHash());
+
+    multimap<int, CComVariant>::iterator it_wf (pair_range.first);
+    for (; it_wf != pair_range.second; ++it_wf)
+    {
+        if ((*it_wf).first != co_Hasher.i_GramHash())
+        {
+            ATLASSERT(0);
+            wstring str_msg (L"Error extracting map element.");
+            ERROR_LOG (str_msg);
+            throw CT_Exception (E_INVALIDARG, str_msg);
+        }
+
+        CComQIPtr<IWordForm> sp_wf = (*it_wf).second.pdispVal;
+        if (NULL == sp_wf)
+        {
+            ATLASSERT(0);
+            wstring str_msg (L"QI for IWordForm failed.");
+            ERROR_LOG (str_msg);
+            throw CT_Exception (E_POINTER, str_msg);
+        }
+
+        m_coll.push_back ((*it_wf).second.pdispVal);
+    }
+
+}   //  v_FormsFromHash()
---
+++ MainLib/FormDescriptor.h
@@ -0,0 +1,116 @@
+#pragma once
+#include "resource.h"       // main symbols
+#include "MainLib_i.h"
+
+#include "VCUE_Collection.h"
+#include "VCUE_Copy.h"
+
+#include "WordForm.h"
+#include "GramHasher.h"
+
+using namespace std;
+using namespace std::tr1;
+
+#if defined(_WIN32_WCE) && !defined(_CE_DCOM) && !defined(_CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA)
+#error "Single-threaded COM objects are not properly supported on Windows CE platform, such as the Windows Mobile platforms that do not include full DCOM support. Define _CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA to force ATL to support creating single-thread COM object's and allow use of it's single-threaded COM object implementations. The threading model in your rgs file was set to 'Free' as that is the only threading model supported in non DCOM Windows CE platforms."
+#endif
+
+namespace WordFormVector
+{
+// *** This was taken from ATLCollections Sample
+// I'm not using some of the typedefs listed below 
+// as they only make this convoluted code even more obscure.
+// However, the MSFT's typedef names appear in comments. 
+//                                                  -- kb
+
+// We always need to provide the following information
+//typedef std::vector<CComVariant> ContainerType;
+//typedef VARIANT	ExposedType;
+//typedef IEnumVARIANT EnumeratorInterface;
+
+// Typically the copy policy can be calculated from the typedefs defined above:
+//typedef VCUE::GenericCopy<ExposedType, ContainerType::value_type> CopyType;
+// However, we may want to use a different class, as in this case:
+//typedef VCUE::MapCopy<ContainerType, ExposedType>				CopyType;
+	// (The advantage of MapCopy is that we don't need to provide implementations 
+	//  of GenericCopy for all the different pairs of key and value types)
+
+// Now we have all the information we need to fill in the template arguments on the implementation classes
+
+//
+// Vector enumerator
+//
+typedef CComEnumOnSTL <IEnumVARIANT, 
+                       &__uuidof(IEnumVARIANT), 
+                       VARIANT,
+                       VCUE::GenericCopy<VARIANT, 
+                                         std::vector<CComVariant>::value_type>,
+                       std::vector<CComVariant> > VectorEnumeratorType;
+
+//
+// Vector-based container
+//
+typedef VCUE::ICollectionOnSTLCopyImpl <IFormDescriptor,
+                                        std::vector<CComVariant>, 
+                                        VARIANT,
+                                        VCUE::GenericCopy<VARIANT, 
+                                                          std::vector<CComVariant>::value_type>,
+                                        VectorEnumeratorType> VectorCollection;
+
+}   // namespace WordFormVector
+
+using namespace WordFormVector;
+
+class ATL_NO_VTABLE CT_FormDescriptor :
+	public CComObjectRootEx<CComSingleThreadModel>,
+    public CComCoClass<CT_FormDescriptor, &CLSID_ZalFormDescriptor>,
+    public IDispatchImpl<VectorCollection, &IID_IFormDescriptor, &LIBID_MainLib, /*wMajor =*/ 1, /*wMinor =*/ 0>
+{
+friend class CT_Lexeme;
+
+public:
+	CT_FormDescriptor()
+	{
+	}
+
+//DECLARE_REGISTRY_RESOURCEID(IDR_LEXEME)
+    DECLARE_NO_REGISTRY()
+
+    BEGIN_COM_MAP(CT_FormDescriptor)
+	COM_INTERFACE_ENTRY(IFormDescriptor)
+    COM_INTERFACE_ENTRY(IDispatch)
+END_COM_MAP()
+
+	DECLARE_PROTECT_FINAL_CONSTRUCT()
+
+	HRESULT FinalConstruct()
+	{
+        return S_OK;
+    }
+
+	void FinalRelease()
+	{}
+
+    STDMETHOD (FindForms) ();
+    STDMETHOD (put_PartOfSpeech) (ET_PartOfSpeech);
+    STDMETHOD (put_Subparadigm) (ET_Subparadigm);
+    STDMETHOD (put_Case) (ET_Case);
+    STDMETHOD (put_Number) (ET_Number);
+    STDMETHOD (put_Gender) (ET_Gender);
+    STDMETHOD (put_Tense) (ET_Tense);
+    STDMETHOD (put_Person) (ET_Person);
+    STDMETHOD (put_Animacy) (ET_Animacy);
+    STDMETHOD (put_Reflexivity) (ET_Reflexive);
+    STDMETHOD (put_Voice) (ET_Voice);
+    STDMETHOD (put_Aspect) (ET_Aspect);
+
+protected:
+    void v_FormsFromHash();
+
+private:
+    CT_GramHasher co_Hasher;
+    CComPtr<CT_Lexeme> sp_Lexeme;
+
+};
+
+OBJECT_ENTRY_AUTO(__uuidof(ZalFormDescriptor), CT_FormDescriptor)
