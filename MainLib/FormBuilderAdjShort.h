59fd23159ee159ea6a6522d9af1f2ff83f6b7d35
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2010-04-23 05:01:10 +0000

    Misc. changes to enable handling of short participles.

--- MainLib/FormBuilderAdjShort.h
+++ MainLib/FormBuilderAdjShort.h
@@ -14,9 +14,11 @@ public:
     CT_FormBuilderShortAdj (CT_Lexeme * pco_lexeme) : 
         CT_FormBuilderBaseDecl (pco_lexeme),
         str_Lemma (pco_lexeme->xstr_GraphicStem.c_str()),
+        eo_Subparadigm (SUBPARADIGM_SHORT_ADJ),
         i_Type (pco_lexeme->i_Type),
         eo_AccentType1 (pco_Lexeme->eo_AccentType1),
         eo_AccentType2 (pco_Lexeme->eo_AccentType2),
+        b_FleetingVowel (pco_Lexeme->b_FleetingVowel),
         map_CommonDeviations (pco_lexeme->map_CommonDeviations)
     {
         if (AT_UNDEFINED == eo_AccentType2)
@@ -44,7 +46,6 @@ public:
                     wstring str_msg (L"Accent type undefined for short forms.");
                     ERROR_LOG (str_msg);
                     throw CT_Exception (E_FAIL, str_msg);
-
                 }
             }       // switch
         }
@@ -57,15 +58,33 @@ public:
     CT_FormBuilderShortAdj (CT_Lexeme * pco_lexeme, 
                             bool b_yoAlternation, 
                             const wstring& str_lemma,
+                            ET_Subparadigm eo_subparadigm,
                             ET_AccentType eo_accentType1,
-                            ET_AccentType eo_accentType2) : 
-        CT_FormBuilderBaseDecl (pco_lexeme, b_yoAlternation), 
+                            ET_AccentType eo_accentType2,
+                            vector<int>& vec_iStressPos,
+                            bool b_fleetingVowel,
+                            int i_deviation = -1) : 
+        CT_FormBuilderBaseDecl (pco_lexeme, b_fleetingVowel, b_yoAlternation), 
         str_Lemma (str_lemma), 
+        eo_Subparadigm (eo_subparadigm),
         i_Type (1),
+        vec_IStressPos (vec_iStressPos),
         eo_AccentType1 (eo_accentType1),
-        eo_AccentType2 (eo_accentType2)
+        eo_AccentType2 (eo_accentType2),
+        b_FleetingVowel (b_fleetingVowel)
     {
         pco_Endings = new CT_AdjShortEndings();
+        if (i_deviation > 0)
+        {
+            if (i_deviation != 2)
+            {
+                ATLASSERT(0);
+                wstring str_msg (L"Unexpected common deviation in short participle.");
+                ERROR_LOG (str_msg);
+                throw CT_Exception (E_INVALIDARG, str_msg);
+            }
+            map_CommonDeviations[i_deviation] = false; // not optional
+        }
     }
 
     HRESULT h_GetEndings();
@@ -93,9 +112,12 @@ public:
     HRESULT h_Build();
 
 private:
+    ET_Subparadigm eo_Subparadigm;
     wstring str_Lemma;
     int i_Type;
+    vector<int> vec_IStressPos;        // Participles only
     ET_AccentType eo_AccentType1, eo_AccentType2;
+    bool b_FleetingVowel;
     map<int, bool> map_CommonDeviations;
 
 };
