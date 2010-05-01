0bbd466429a394002686710e4f7f7400202fbab0
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2010-05-01 16:50:26 +0000

    Change ctor.

--- MainLib/FormBuilderAdjComparative.h
+++ MainLib/FormBuilderAdjComparative.h
@@ -7,7 +7,8 @@ using namespace std::tr1;
 class CT_FormBuilderComparative : public CT_FormBuilderBaseDecl
 {
 public:
-    CT_FormBuilderComparative (CT_Lexeme * pco_lexeme) : CT_FormBuilderBaseDecl (pco_lexeme)
+    CT_FormBuilderComparative (CT_Lexeme * pco_lexeme) 
+        : CT_FormBuilderBaseDecl (pco_lexeme, SUBPARADIGM_COMPARATIVE)
     {}
 
 public:
