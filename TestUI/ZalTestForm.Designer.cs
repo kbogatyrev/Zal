5ed9225a5e70440cf701a21effc12bd01be85a5b
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2010-05-16 04:27:18 +0000

    Implement saving of test data.

--- TestUI/ZalTestForm.Designer.cs
+++ TestUI/ZalTestForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.tabSearch = new System.Windows.Forms.TabPage();
             this.panelLex = new System.Windows.Forms.Panel();
+            this.radioButtonPreprocess = new System.Windows.Forms.RadioButton();
             this.buttonSelect = new System.Windows.Forms.Button();
             this.textBoxDBPath = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -42,7 +43,6 @@
             this.textBoxSearchString = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.tabControl = new System.Windows.Forms.TabControl();
-            this.radioButtonPreprocess = new System.Windows.Forms.RadioButton();
             this.tabSearch.SuspendLayout();
             this.panelLex.SuspendLayout();
             this.tabControl.SuspendLayout();
@@ -54,7 +54,7 @@
             this.tabSearch.Location = new System.Drawing.Point(4, 22);
             this.tabSearch.Name = "tabSearch";
             this.tabSearch.Padding = new System.Windows.Forms.Padding(3);
-            this.tabSearch.Size = new System.Drawing.Size(694, 818);
+            this.tabSearch.Size = new System.Drawing.Size(694, 759);
             this.tabSearch.TabIndex = 0;
             this.tabSearch.Text = "Search";
             this.tabSearch.UseVisualStyleBackColor = true;
@@ -78,12 +78,23 @@
             this.panelLex.Size = new System.Drawing.Size(688, 859);
             this.panelLex.TabIndex = 0;
             // 
+            // radioButtonPreprocess
+            // 
+            this.radioButtonPreprocess.AutoSize = true;
+            this.radioButtonPreprocess.Location = new System.Drawing.Point(457, 94);
+            this.radioButtonPreprocess.Name = "radioButtonPreprocess";
+            this.radioButtonPreprocess.Size = new System.Drawing.Size(78, 17);
+            this.radioButtonPreprocess.TabIndex = 6;
+            this.radioButtonPreprocess.TabStop = true;
+            this.radioButtonPreprocess.Text = "Preprocess";
+            this.radioButtonPreprocess.UseVisualStyleBackColor = true;
+            // 
             // buttonSelect
             // 
             this.buttonSelect.Location = new System.Drawing.Point(574, 59);
             this.buttonSelect.Name = "buttonSelect";
             this.buttonSelect.Size = new System.Drawing.Size(75, 23);
-            this.buttonSelect.TabIndex = 10;
+            this.buttonSelect.TabIndex = 2;
             this.buttonSelect.Text = "Select...";
             this.buttonSelect.UseVisualStyleBackColor = true;
             this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
@@ -93,7 +104,7 @@
             this.textBoxDBPath.Location = new System.Drawing.Point(174, 58);
             this.textBoxDBPath.Name = "textBoxDBPath";
             this.textBoxDBPath.Size = new System.Drawing.Size(384, 20);
-            this.textBoxDBPath.TabIndex = 9;
+            this.textBoxDBPath.TabIndex = 1;
             this.textBoxDBPath.TextChanged += new System.EventHandler(this.textBoxDBPath_TextChanged);
             // 
             // textBox2
@@ -103,7 +114,8 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(74, 20);
-            this.textBox2.TabIndex = 8;
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TabStop = false;
             this.textBox2.Text = "DB Path:";
             // 
             // radioButtonWordForm
@@ -112,7 +124,7 @@
             this.radioButtonWordForm.Location = new System.Drawing.Point(360, 94);
             this.radioButtonWordForm.Name = "radioButtonWordForm";
             this.radioButtonWordForm.Size = new System.Drawing.Size(91, 17);
-            this.radioButtonWordForm.TabIndex = 7;
+            this.radioButtonWordForm.TabIndex = 5;
             this.radioButtonWordForm.TabStop = true;
             this.radioButtonWordForm.Text = "Random Form";
             this.radioButtonWordForm.UseVisualStyleBackColor = true;
@@ -124,14 +136,14 @@
             this.lexPanel.Location = new System.Drawing.Point(23, 167);
             this.lexPanel.Name = "lexPanel";
             this.lexPanel.Size = new System.Drawing.Size(639, 676);
-            this.lexPanel.TabIndex = 6;
+            this.lexPanel.TabIndex = 0;
             // 
             // buttonLookup
             // 
             this.buttonLookup.Location = new System.Drawing.Point(574, 126);
             this.buttonLookup.Name = "buttonLookup";
             this.buttonLookup.Size = new System.Drawing.Size(75, 23);
-            this.buttonLookup.TabIndex = 5;
+            this.buttonLookup.TabIndex = 8;
             this.buttonLookup.Text = "Lookup";
             this.buttonLookup.UseVisualStyleBackColor = true;
             this.buttonLookup.Click += new System.EventHandler(this.buttonLookup_Click);
@@ -143,7 +155,8 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
             this.textBox3.Size = new System.Drawing.Size(74, 20);
-            this.textBox3.TabIndex = 4;
+            this.textBox3.TabIndex = 0;
+            this.textBox3.TabStop = false;
             this.textBox3.Text = "Search By:";
             // 
             // radioButtonInitForm
@@ -152,7 +165,7 @@
             this.radioButtonInitForm.Location = new System.Drawing.Point(268, 94);
             this.radioButtonInitForm.Name = "radioButtonInitForm";
             this.radioButtonInitForm.Size = new System.Drawing.Size(86, 17);
-            this.radioButtonInitForm.TabIndex = 3;
+            this.radioButtonInitForm.TabIndex = 4;
             this.radioButtonInitForm.TabStop = true;
             this.radioButtonInitForm.Text = "Entry Source";
             this.radioButtonInitForm.UseVisualStyleBackColor = true;
@@ -163,7 +176,7 @@
             this.radioButtonGStem.Location = new System.Drawing.Point(173, 94);
             this.radioButtonGStem.Name = "radioButtonGStem";
             this.radioButtonGStem.Size = new System.Drawing.Size(89, 17);
-            this.radioButtonGStem.TabIndex = 2;
+            this.radioButtonGStem.TabIndex = 3;
             this.radioButtonGStem.TabStop = true;
             this.radioButtonGStem.Text = "Graphic Stem";
             this.radioButtonGStem.UseVisualStyleBackColor = true;
@@ -173,7 +186,7 @@
             this.textBoxSearchString.Location = new System.Drawing.Point(174, 128);
             this.textBoxSearchString.Name = "textBoxSearchString";
             this.textBoxSearchString.Size = new System.Drawing.Size(384, 20);
-            this.textBoxSearchString.TabIndex = 1;
+            this.textBoxSearchString.TabIndex = 7;
             this.textBoxSearchString.TextChanged += new System.EventHandler(this.textBoxSearchString_TextChanged);
             // 
             // textBox1
@@ -184,6 +197,7 @@
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(124, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
             this.textBox1.Text = "Enter Search String:";
             // 
             // tabControl
@@ -193,25 +207,14 @@
             this.tabControl.Location = new System.Drawing.Point(0, 0);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(702, 844);
+            this.tabControl.Size = new System.Drawing.Size(702, 785);
             this.tabControl.TabIndex = 0;
-            // 
-            // radioButtonPreprocess
-            // 
-            this.radioButtonPreprocess.AutoSize = true;
-            this.radioButtonPreprocess.Location = new System.Drawing.Point(457, 94);
-            this.radioButtonPreprocess.Name = "radioButtonPreprocess";
-            this.radioButtonPreprocess.Size = new System.Drawing.Size(78, 17);
-            this.radioButtonPreprocess.TabIndex = 11;
-            this.radioButtonPreprocess.TabStop = true;
-            this.radioButtonPreprocess.Text = "Preprocess";
-            this.radioButtonPreprocess.UseVisualStyleBackColor = true;
             // 
             // TestApplet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(702, 844);
+            this.ClientSize = new System.Drawing.Size(702, 785);
             this.Controls.Add(this.tabControl);
             this.Name = "TestApplet";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
