9cdd99d4caac6f4dc27389f276529e17dd1c01af
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2010-06-04 04:32:02 +0000

    Add value.

--- MainLib/resource.h
+++ MainLib/resource.h
@@ -7,15 +7,15 @@
 #define IDR_DICTIONARY                  102
 #define IDR_LEXEME                      103
 #define IDR_ANALYZER                    104
-
+#define IDR_TESTDATA                    105
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        201
+#define _APS_NEXT_RESOURCE_VALUE        204
 #define _APS_NEXT_COMMAND_VALUE         32768
 #define _APS_NEXT_CONTROL_VALUE         201
-#define _APS_NEXT_SYMED_VALUE           103
+#define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
