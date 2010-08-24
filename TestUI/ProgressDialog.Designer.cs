ba711539db3b0e186797a2f974c8f3afb60104b8
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2010-08-24 06:01:10 +0000

    Initial revision.

---
+++ TestUI/ProgressDialog.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TestUI
+{
+    partial class ProgressDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(13, 66);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(335, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(273, 103);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 2;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.textBox.Location = new System.Drawing.Point(13, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(335, 37);
+            this.textBox.TabIndex = 3;
+            // 
+            // ProgressDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(360, 135);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.progressBar);
+            this.Name = "ProgressDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ProgressDialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
---
+++ TestUI/ProgressDialog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestUI
+{
+    public partial class ProgressDialog : Form
+    {
+        public ProgressDialog()
+        {
+            InitializeComponent();
+        }
+
+        public void SetProgressBarPos (int iPercent)
+        {
+            progressBar.Value = iPercent;
+        }
+
+        public void SetMessage(string sMsg)
+        {
+            textBox.Text = sMsg;
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
