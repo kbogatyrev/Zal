c14d07571b2de1b1271c20a1cec935567e91c07f
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2010-09-04 20:15:13 +0000

    Add resource id's.

--- MainLib/Resource.h
+++ MainLib/Resource.h
@@ -11,6 +11,7 @@
 #define IDR_VERIFIER                    106
 #define IDR_WORDFORM                    108
 #define IDR_SQLITEWRAPPER               109
+#define IDR_ERROR                       110
 
 // Next default values for new objects
 // 
@@ -19,6 +20,6 @@
 #define _APS_NEXT_RESOURCE_VALUE        204
 #define _APS_NEXT_COMMAND_VALUE         32768
 #define _APS_NEXT_CONTROL_VALUE         201
-#define _APS_NEXT_SYMED_VALUE           110
+#define _APS_NEXT_SYMED_VALUE           111
 #endif
 #endif
