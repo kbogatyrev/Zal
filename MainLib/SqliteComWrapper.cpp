0e5db3996c6ac114c345b2ef72b11071886fe88e
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2010-09-04 20:13:45 +0000

    Change import/export code to handle multiple tables.

--- MainLib/SqliteComWrapper.cpp
+++ MainLib/SqliteComWrapper.cpp
@@ -93,7 +93,87 @@ HRESULT CT_SqliteComWrapper::TableExists (BSTR bstr_table, BOOL * b_exists)
 
 }   //  TableExists (...)
 
-HRESULT CT_SqliteComWrapper::ExportTable (BSTR bstr_path, BSTR bstr_table)
+HRESULT CT_SqliteComWrapper::Export (BSTR bstr_path, SAFEARRAY * sarr_names)
+{
+    USES_CONVERSION;
+
+    HRESULT h_r = S_OK;
+
+    if (NULL == pco_Db)
+    {
+        ATLASSERT(0);
+        ERROR_LOG (L"DB pointer is NULL.");
+        return E_POINTER;
+    }
+
+    CComVariant sp_varName;
+    vector<wstring> vec_tableNames;
+    
+    long l_uBound = 0;
+    long l_lBound = 0;
+    h_r = SafeArrayGetUBound (sarr_names, 1, &l_uBound);
+    if (S_OK != h_r)
+    {
+        ATLASSERT(0);
+        ERROR_LOG (L"Error getting safearray's lower bound.");
+        return E_FAIL;
+    }
+
+    h_r = SafeArrayGetLBound (sarr_names, 1, &l_lBound);
+    if (S_OK != h_r)
+    {
+        ATLASSERT(0);
+        ERROR_LOG (L"Error getting safearray's upper bound.");
+        return E_FAIL;
+    }
+
+    unsigned int i_arrSize = l_uBound - l_lBound + 1;
+    for (unsigned int ui_at = 0; ui_at < i_arrSize; ++ui_at)
+    {
+        CComBSTR bstr_name;
+        h_r = SafeArrayGetElement (sarr_names, (long *)&ui_at, &bstr_name);
+        if (S_OK != h_r)
+        {
+            ATLASSERT(0);
+            ERROR_LOG (L"Error extracting table name from safearray.");
+            return E_FAIL;
+        }
+        vec_tableNames.push_back (OLE2W (bstr_name));
+    }
+
+    try
+    {
+        CT_StatusUpdate co_progress;
+        co_progress.pco_Parent = this;
+        bool b_ret = pco_Db->b_ExportTables  (OLE2W (bstr_path), vec_tableNames, co_progress);
+    }
+    catch (...)
+    {
+        wstring str_msg;
+        try
+        {
+            wstring str_error;
+            pco_Db->v_GetLastError (str_error);
+            str_msg = L"DB error %d: ";
+            str_msg += str_error;
+        }
+        catch (...)
+        {
+            str_msg = L"Apparent DB error ";
+        }
+    
+        CString cs_msg;
+        cs_msg.Format (str_msg.c_str(), pco_Db->i_GetLastError());
+        ERROR_LOG ((LPCTSTR)cs_msg);
+ 
+        return E_FAIL;
+    }
+
+    return S_OK;
+
+}   //  Export (...)
+
+HRESULT CT_SqliteComWrapper::Import (BSTR bstr_path)
 {
     USES_CONVERSION;
 
@@ -110,7 +190,7 @@ HRESULT CT_SqliteComWrapper::ExportTable (BSTR bstr_path, BSTR bstr_table)
     {
         CT_StatusUpdate co_progress;
         co_progress.pco_Parent = this;
-        bool b_ret = pco_Db->b_ExportTable (OLE2W (bstr_path), OLE2W (bstr_table), co_progress);
+        bool b_ret = pco_Db->b_ImportTables (OLE2W (bstr_path), co_progress);
         if (!b_ret)
         {
             return E_FAIL;
@@ -140,53 +220,4 @@ HRESULT CT_SqliteComWrapper::ExportTable (BSTR bstr_path, BSTR bstr_table)
 
     return S_OK;
 
-}   //  ExportTable (...)
-
-HRESULT CT_SqliteComWrapper::ImportTable (BSTR bstr_path, BSTR bstr_table)
-{
-    USES_CONVERSION;
-
-    HRESULT h_r = S_OK;
-
-    if (NULL == pco_Db)
-    {
-        ATLASSERT(0);
-        ERROR_LOG (L"DB pointer is NULL.");
-        return E_POINTER;
-    }
-
-    try
-    {
-        CT_StatusUpdate co_progress;
-        co_progress.pco_Parent = this;
-        bool b_ret = pco_Db->b_ImportTable (OLE2W (bstr_path), OLE2W (bstr_table), co_progress);
-        if (!b_ret)
-        {
-            return E_FAIL;
-        }
-    }
-    catch (...)
-    {
-        wstring str_msg;
-        try
-        {
-            wstring str_error;
-            pco_Db->v_GetLastError (str_error);
-            str_msg = L"DB error %d: ";
-            str_msg += str_error;
-        }
-        catch (...)
-        {
-            str_msg = L"Apparent DB error ";
-        }
-    
-        CString cs_msg;
-        cs_msg.Format (str_msg.c_str(), pco_Db->i_GetLastError());
-        ERROR_LOG ((LPCTSTR)cs_msg);
- 
-        return E_FAIL;
-    }
-
-    return S_OK;
-
-}   //  ImportTable (...)
+}   //  Import (...)
