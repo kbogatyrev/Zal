12650f0e4ce7577dc7680e0e68967025c72cbe4a
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2010-09-27 16:41:50 +0000

    Handle usage status.

--- MainLib/FormBuilderAdjLong.h
+++ MainLib/FormBuilderAdjLong.h
@@ -12,12 +12,14 @@ public:
                            const wstring& str_lemma,
                            ET_AccentType eo_accentType, 
                            ET_Subparadigm eo_subparadigm,
-                           int i_stemStressPos = -1) 
+                           int i_stemStressPos = -1,
+                           ET_Status eo_status = STATUS_COMMON) 
         : CT_FormBuilderBaseDecl (pco_lexeme, SUBPARADIGM_LONG_ADJ),
           str_Lemma (str_lemma),
           eo_AccentType (eo_accentType),
           eo_Subparadigm (eo_subparadigm),
-          i_StemStressPos (i_stemStressPos)
+          i_StemStressPos (i_stemStressPos),
+          eo_Status (eo_status)
     {
         pco_Endings = new CT_AdjLongEndings();
     }
@@ -52,4 +54,5 @@ private:
     ET_AccentType eo_AccentType;
     ET_Subparadigm eo_Subparadigm;
     int i_StemStressPos;        // participles only
+    ET_Status eo_Status;
 };
