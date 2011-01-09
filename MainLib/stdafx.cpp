ad676ea690853a77547339317b03252aa465d4d5
kostya <kostya@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2011-01-09 21:38:27 +0000

    Add static initializers.

--- MainLib/stdafx.cpp
+++ MainLib/stdafx.cpp
@@ -3,3 +3,7 @@
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
+
+vector<wstring> CT_Error::vec_Log_;
+sqlite3 * CT_Sqlite::spo_Db = NULL;
+int CT_Sqlite::si_Refcount = 0;
