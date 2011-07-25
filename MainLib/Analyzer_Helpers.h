2b8340a1b04ef30b1931b5a3136fce5281e80727
timothei <timothei@cc0ef63a-adc4-4bcc-ac96-f7243395e8df>
2011-07-25 10:34:56 +0000

    CEndingsTable: binary search and use of vectors instead of DB.

--- MainLib/Analyzer_Helpers.h
+++ MainLib/Analyzer_Helpers.h
@@ -14,22 +14,38 @@ public:
     CEString sLemma;
 };
 
+struct stInflexion
+{
+    CEString sEnding;
+    int iGram;
+    int iStress;
+
+    friend bool operator< (stInflexion& soInfl1, stInflexion& soInfl2);
+    friend bool operator> (stInflexion& soInfl1, stInflexion& soInfl2);
+    friend bool operator== (stInflexion& soInfl1, stInflexion& soInfl2);
+};
+
 class CEndingsTable
 {
+private:
+    bool bSorted;
+
 public:
-    vector<CEString> m_vecEndings;
-    vector<int> m_vecGram;
-    vector<int> m_vecStress;
+    vector<stInflexion> m_vecEndings;
 
     int m_iLexemeCount, m_iCutRight;
     CEString m_sStemFinal, m_sLemmaFinal;
-    vector<int> m_vecFind (CEString sEnding, int iStressPos = -2);
-    vector<int> vecDbId (CSqlite* pDbHandle); // Tries to find the table in the db, returns -1 if there's no such table
+    vector<int> m_vecFind(CEString sEnding, int iStressPos = -2);
+    vector<int> vecDbId(CSqlite* pDbHandle); // Tries to find the table in the db, returns -1 if there's no such table
+    int iDbId(vector<CEndingsTable>& vecDb);
 
     CEndingsTable();
+    CEndingsTable(const CEndingsTable& Source);
     ~CEndingsTable();
     void Clear();
-    int iAddEnding (int iHash, CEString sEnding, int iStressPos);
-    int iAddEnding (long lHash, CEString sEnding, int iStressPos);
-    int iDbWrite (CSqlite* pDbHandle); // Write the whole table to the db, return subtable_id
+    void vSort(); // Sorts all inflexions (ordered by ending, then by iStress, then by iGram)
+    int iAddEnding(int iHash, CEString sEnding, int iStressPos);
+    int iAddEnding(long lHash, CEString sEnding, int iStressPos);
+    int iDbWrite(CSqlite* pDbHandle, bool bSearchFirst = true, int iSubtableId = -1); // Write the whole table to the db, return subtable_id
+    int iDbWrite(vector<CEndingsTable>& vecDb, bool bSearchFirst = true);
 };
