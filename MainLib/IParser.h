980aec6f7f21054d6a81d7dcf4ff81aad248d3be
Konstantin Bogatyrev <kbogatyrev@gopro.com>
2014-11-09 07:08:52 -0800

    Initial revision.

---
+++ MainLib/IParser.h
@@ -0,0 +1,18 @@
+#ifndef I_PARSER_H_INCLUDED
+#define I_PARSER_H_INCLUDED
+
+#include "Enums.h"
+#include "IWordForm.h"
+
+namespace Hlib
+{
+    struct IParser
+    {
+        virtual ET_ReturnCode eAnalyze(const CEString& sText) = 0;
+        virtual ET_ReturnCode eGetFirstWordForm(IWordForm *& pWordForm) = 0;
+        virtual ET_ReturnCode eGetNextWordForm(IWordForm *& pWordForm) = 0;
+    };
+
+}   // namespace Hlib
+
+#endif  //  I_PARSER_H_INCLUDED
---
+++ MainLib/Parser.h
@@ -0,0 +1,42 @@
+#ifndef C_PARSER_H_INCLUDED
+#define C_PARSER_H_INCLUDED
+
+#include "IParser.h"
+
+namespace Hlib
+{
+
+class CSqlite;
+class CParsingTree;
+struct CWordForm;
+
+class CParser : public IParser
+{
+
+public:
+    CParser(CSqlite *);
+    ~CParser();
+
+public:
+    virtual ET_ReturnCode eAnalyze(const CEString& sWord);
+    virtual ET_ReturnCode eGetFirstWordForm(IWordForm *& pWordForm);
+    virtual ET_ReturnCode eGetNextWordForm(IWordForm *& pWordForm);
+
+private:
+    CParser();  // no use
+
+    CSqlite * m_pDb;
+    CParsingTree * m_pEndingsTree;
+    vector<CWordForm *> m_vecWordForms;
+    vector<CWordForm *>::iterator m_itCurrentWordForm;
+
+
+    ET_ReturnCode eIrregularFormLookup(const CEString&);
+    ET_ReturnCode eFormLookup(const CEString&);
+    ET_ReturnCode eQueryDb(const CEString& sSelect, unsigned int& uiQueryHandle);
+    void HandleDbException(CException& ex);
+};
+
+}   // namespace Hlib
+
+#endif // C_PARSER_H_INCLUDED
