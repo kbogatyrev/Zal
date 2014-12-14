8aeb7999c8ebc6b9109f1c9abfefb483ed262f7c
Konstantin Bogatyrev <kbogatyrev@gopro.com>
2014-12-14 10:53:20 -0800

    Partial refactoring of regular form lookup; bug fuxes, add infinitives.

--- MainLib/Endings.h
+++ MainLib/Endings.h
@@ -17,6 +17,7 @@ class CLexeme;
 struct StEndingDescriptor
 {
     __int64 llDbKey;
+    ET_Subparadigm eSubparadigm;
     int iInflectionType;
     ET_Gender eGender;
     ET_Number eNumber;
@@ -27,6 +28,7 @@ struct StEndingDescriptor
     ET_StemAuslaut eStemAuslaut;
 
     StEndingDescriptor() : llDbKey(-1),
+                           eSubparadigm(SUBPARADIGM_UNDEFINED),
                            iInflectionType (INFLECTION_TYPE_UNDEFINED),
                            eGender (GENDER_UNDEFINED), 
                            eNumber (NUM_UNDEFINED), 
@@ -37,19 +39,33 @@ struct StEndingDescriptor
                            eStemAuslaut (STEM_AUSLAUT_UNDEFINED)
     {}
 
+    StEndingDescriptor(ET_Subparadigm eSubparadigm) :
+                            llDbKey(-1),
+                            eSubparadigm(eSubparadigm),
+                            iInflectionType(INFLECTION_TYPE_UNDEFINED),
+                            eGender(GENDER_UNDEFINED),
+                            eNumber(NUM_UNDEFINED),
+                            eCase(CASE_UNDEFINED),
+                            eAnimacy(ANIM_UNDEFINED),
+                            ePerson(PERSON_UNDEFINED),
+                            eStress(STRESS_LOCATION_UNDEFINED),
+                            eStemAuslaut(STEM_AUSLAUT_UNDEFINED)
+    {}
+
     // Overload for nouns
     StEndingDescriptor (ET_Number eN, 
                         ET_Case eC, 
                         ET_StressLocation eS) :
                             llDbKey(-1),
-                            iInflectionType (INFLECTION_TYPE_UNDEFINED),
-                            eGender (GENDER_UNDEFINED), 
-                            eNumber (eN),
-                            eCase (eC), 
-                            eAnimacy (ANIM_UNDEFINED), 
-                            ePerson (PERSON_UNDEFINED),
-                            eStress (eS),
-                            eStemAuslaut (STEM_AUSLAUT_UNDEFINED)
+                            eSubparadigm(SUBPARADIGM_NOUN),
+                            iInflectionType(INFLECTION_TYPE_UNDEFINED),
+                            eGender(GENDER_UNDEFINED), 
+                            eNumber(eN),
+                            eCase(eC), 
+                            eAnimacy(ANIM_UNDEFINED), 
+                            ePerson(PERSON_UNDEFINED),
+                            eStress(eS),
+                            eStemAuslaut(STEM_AUSLAUT_UNDEFINED)
     {}
 
     // Overload for long adjectves
@@ -59,14 +75,15 @@ struct StEndingDescriptor
                         ET_Animacy eA,
                         ET_StressLocation eS) :
                             llDbKey(-1),
-                            iInflectionType (INFLECTION_TYPE_UNDEFINED),
-                            eGender (eG), 
-                            eNumber (eN),
-                            eCase (eC),
-                            eAnimacy (eA),
-                            ePerson (PERSON_UNDEFINED),
-                            eStress (eS),
-                            eStemAuslaut (STEM_AUSLAUT_UNDEFINED)
+                            eSubparadigm(SUBPARADIGM_LONG_ADJ),
+                            iInflectionType(INFLECTION_TYPE_UNDEFINED),
+                            eGender(eG), 
+                            eNumber(eN),
+                            eCase(eC),
+                            eAnimacy(eA),
+                            ePerson(PERSON_UNDEFINED),
+                            eStress(eS),
+                            eStemAuslaut(STEM_AUSLAUT_UNDEFINED)
     {}
 
     // Overload for short adjectives
@@ -74,14 +91,15 @@ struct StEndingDescriptor
                         ET_Number eN,
                         ET_StressLocation eS) :
                             llDbKey(-1),
-                            iInflectionType (INFLECTION_TYPE_UNDEFINED),
-                            eGender (eG), 
-                            eNumber (eN),
-                            eCase (CASE_UNDEFINED), 
-                            eAnimacy (ANIM_UNDEFINED), 
-                            ePerson (PERSON_UNDEFINED),
-                            eStress (eS),
-                            eStemAuslaut (STEM_AUSLAUT_UNDEFINED)
+                            eSubparadigm(SUBPARADIGM_SHORT_ADJ),
+                            iInflectionType(INFLECTION_TYPE_UNDEFINED),
+                            eGender(eG), 
+                            eNumber(eN),
+                            eCase(CASE_UNDEFINED), 
+                            eAnimacy(ANIM_UNDEFINED), 
+                            ePerson(PERSON_UNDEFINED),
+                            eStress(eS),
+                            eStemAuslaut(STEM_AUSLAUT_UNDEFINED)
     {}
 
     // Overload for personal forms
@@ -91,14 +109,15 @@ struct StEndingDescriptor
                         ET_StressLocation eS,
                         ET_StemAuslaut eSa) :
                             llDbKey(-1),
-                            iInflectionType (iInflectionType),
-                            eGender (GENDER_UNDEFINED),
-                            eNumber (eN),
-                            eCase (CASE_UNDEFINED),
-                            eAnimacy (ANIM_UNDEFINED),
-                            ePerson (eP), 
-                            eStress (eS),
-                            eStemAuslaut (eSa)
+                            eSubparadigm(SUBPARADIGM_PRESENT_TENSE),
+                            iInflectionType(iInflectionType),
+                            eGender(GENDER_UNDEFINED),
+                            eNumber(eN),
+                            eCase(CASE_UNDEFINED),
+                            eAnimacy(ANIM_UNDEFINED),
+                            ePerson(eP), 
+                            eStress(eS),
+                            eStemAuslaut(eSa)
     {}
 
     // Overload for past tense forms
@@ -106,6 +125,7 @@ struct StEndingDescriptor
                        ET_Number eN,
                        ET_StemAuslaut eA) : 
                             llDbKey(-1),
+                            eSubparadigm(SUBPARADIGM_PAST_TENSE),
                             iInflectionType(INFLECTION_TYPE_UNDEFINED),
                             eGender(eG),
                             eNumber(eN),
@@ -138,6 +158,21 @@ struct StEndingDescriptor
         return s_;
     }
 
+    __int64 llHash() const
+    {
+        __int64 llHash = iInflectionType * SUBPARADIGM_COUNT * GENDER_COUNT * NUM_COUNT * CASE_COUNT * ANIM_COUNT * PERSON_COUNT * STRESS_LOCATION_COUNT * STEM_AUSLAUT_COUNT +
+                         eSubparadigm *                        GENDER_COUNT * NUM_COUNT * CASE_COUNT * ANIM_COUNT * PERSON_COUNT * STRESS_LOCATION_COUNT * STEM_AUSLAUT_COUNT +
+                         eGender *                                            NUM_COUNT * CASE_COUNT * ANIM_COUNT * PERSON_COUNT * STRESS_LOCATION_COUNT * STEM_AUSLAUT_COUNT +
+                         eNumber *                                                        CASE_COUNT * ANIM_COUNT * PERSON_COUNT * STRESS_LOCATION_COUNT * STEM_AUSLAUT_COUNT +
+                         eCase  *                                                                      ANIM_COUNT * PERSON_COUNT * STRESS_LOCATION_COUNT * STEM_AUSLAUT_COUNT +
+                         eAnimacy *                                                                                 PERSON_COUNT * STRESS_LOCATION_COUNT * STEM_AUSLAUT_COUNT +
+                         ePerson *                                                                                                 STRESS_LOCATION_COUNT * STEM_AUSLAUT_COUNT +
+                         eStress *                                                                                                                         STEM_AUSLAUT_COUNT +
+                         eStemAuslaut;
+
+        return llHash;
+    }
+
 };      //  struct StEndingDescriptor
 
 //
@@ -146,7 +181,7 @@ struct StEndingDescriptor
 struct StSplit
 {
     unsigned int uiEndingLength;
-    vector<StEndingDescriptor> vecDescriptors;
+    vector<__int64> vecEndingKeys;
 };
 
 //
@@ -157,7 +192,6 @@ struct StNode
     wchar_t cLetter;
     StNode * pPrevious;
     vector<StNode *> vecChildren;
-    vector<StEndingDescriptor> vecTerms;    // endings whose 1st (leftmost) character is cLetter
 };
 
 struct StReverseComparisonFunctor
@@ -224,24 +258,25 @@ public:
 
 public:
     void eSplit(const CEString& sWord);
-    ET_ReturnCode eGetFirstMatch(StSplit& stSplit);
-    ET_ReturnCode eGetNextMatch(StSplit& stSplit);
+    ET_ReturnCode eGetFirstMatch(int& iEndingLength);
+    ET_ReturnCode eGetNextMatch(int& iEndingLength);
     ET_ReturnCode eIsEmptyEnding(__int64 llEndingId);
 
 private:
-    typedef map<CEString, vector<StEndingDescriptor>, StReverseComparisonFunctor> MapEndingToDescriptors;
-    typedef vector<MapEndingToDescriptors::iterator> VecBucket;
+    typedef map<CEString, vector<__int64>, StReverseComparisonFunctor> MapEndingToDescriptors;
+
 
     MapEndingToDescriptors m_mapSortedEndingsList;
     StNode * m_pRoot;
-    vector<StSplit> m_vecMatches;
-    vector<StSplit>::iterator m_itCurrentMatch;
+    vector<int> m_vecMatches;    // length of ending
+    vector<int>::iterator m_itCurrentMatch;
+    set<CEString, StReverseComparisonFunctor> m_setEndingStrings;
     set<__int64> m_setEmptyEndingIds;
 
 protected:
     CParsingTree() {};
     ET_ReturnCode eLoad(CSqlite * pDb);
-    void CParsingTree::AddLevel(unsigned int uiOffset, StNode * pParent, VecBucket vecBucket);
+    void CParsingTree::AddLevel(unsigned int uiOffset, StNode * pParent, vector<CEString> vecEndings);
     void Traverse(StNode * pRoot, const CEString& sWord);
     ET_ReturnCode eTraverseAndDelete(StNode * pRoot);
 
@@ -293,12 +328,12 @@ public:
         m_mmEndings.clear();
     }
 
-    virtual int iHash (const StEndingDescriptor&) = 0;
+//    virtual __int64 llHash (const StEndingDescriptor&) = 0;
 
     int iCount (const StEndingDescriptor& stD)
     {
-        int iH = iHash (stD);
-        return m_mmEndings.count (iH);
+        __int64 llH = stD.llHash();
+        return m_mmEndings.count (llH);
     }
 
     virtual ET_ReturnCode eLoad() = 0;
@@ -306,9 +341,9 @@ public:
 
     ET_ReturnCode eGetEnding (const StEndingDescriptor& stDescriptor, int iSeqNum, CEString& sEnding, __int64& llEndingKey)
     {
-        int iKey = iHash (stDescriptor);
+        __int64 llKey = stDescriptor.llHash();
 
-        ItPair pair_Range = m_mmEndings.equal_range (iKey);
+        ItPair pair_Range = m_mmEndings.equal_range (llKey);
         for (int iItem = 0; iItem < iSeqNum; ++iItem)
         {
             if (++pair_Range.first == pair_Range.second)
@@ -334,9 +369,9 @@ public:
 
 protected:
     void ReportDbError();
-    std::multimap<int, pair<CEString, __int64> > m_mmEndings;       // ending hash to ending data
+    std::multimap<__int64, pair<CEString, __int64> > m_mmEndings;       // ending hash (64-bit) to ending data
 
-    typedef std::multimap<int, pair<CEString, __int64> > EndingsMultiMap;
+    typedef std::multimap<__int64, pair<CEString, __int64> > EndingsMultiMap;
     typedef pair<EndingsMultiMap::const_iterator, EndingsMultiMap::const_iterator> ItPair;
 
     CLexeme * m_pLexeme;
@@ -352,7 +387,7 @@ public:
     virtual ET_ReturnCode eAddEnding (const CEString&, const StEndingDescriptor&);
 
 private:
-    virtual int iHash (const StEndingDescriptor&);
+//    virtual __int64 llHash(const StEndingDescriptor&);
 };
 
 class CAdjLongEndings : public CEndings
@@ -364,7 +399,7 @@ public:
     virtual ET_ReturnCode eAddEnding (const CEString&, const StEndingDescriptor&);
 
 private:
-    virtual int iHash (const StEndingDescriptor&);
+//    virtual __int64 llHash (const StEndingDescriptor&);
 
     ET_Subparadigm m_eSubparadigm;
 };
@@ -378,7 +413,7 @@ public:
     virtual ET_ReturnCode eAddEnding (const CEString&, const StEndingDescriptor&);
 
 private:
-    virtual int iHash (const StEndingDescriptor&);
+//    virtual __int64 llHash(const StEndingDescriptor&);
 };
 
 class CPersonalEndings : public CEndings
@@ -390,7 +425,19 @@ public:
     virtual ET_ReturnCode eAddEnding (const CEString&, const StEndingDescriptor&);
 
 private:
-    virtual int iHash (const StEndingDescriptor&);
+//    virtual __int64 llHash(const StEndingDescriptor&);
+};
+
+class CInfinitiveEndings : public CEndings
+{
+public:
+    CInfinitiveEndings(CLexeme * pLexeme);
+
+    virtual ET_ReturnCode eLoad();
+    virtual ET_ReturnCode eAddEnding(const CEString&, const StEndingDescriptor&);
+
+private:
+//    virtual __int64 llHash(const StEndingDescriptor&);
 };
 
 class CPastTenseEndings : public CEndings
@@ -402,9 +449,10 @@ public:
     virtual ET_ReturnCode eAddEnding(const CEString&, const StEndingDescriptor&);
 
 private:
-    virtual int iHash(const StEndingDescriptor&);
+//    virtual __int64 llHash(const StEndingDescriptor&);
 };
 
+/*
 class CNullEnding : public CEndings
 {
 public:
@@ -416,6 +464,7 @@ public:
 private:
     virtual int iHash(const StEndingDescriptor&);
 };
+*/
 
 } // namespace Hlib
 
