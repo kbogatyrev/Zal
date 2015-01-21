e591707a27008f06574b1b7cdc01480f2f266b3f
Konstantin Bogatyrev <kbogatyrev@gopro.com>
2015-01-21 08:17:28 -0800

    Initial revision.

---
+++ MainLib/ParsingTree.cpp
@@ -0,0 +1,263 @@
+﻿#include "StdAfx.h"
+#include "ParsingTree.h"
+
+using namespace Hlib;
+
+static CEString sSelectBase
+(L"SELECT DISTINCT d.id, e.ending_string, number, case_value, stress FROM endings AS e INNER JOIN ending_data AS d ON (e.id = d.ending_id)");
+
+CParsingTree::CParsingTree(CSqlite * pDb)
+{
+    if (NULL == pDb)
+    {
+        throw CException(H_ERROR_POINTER, L"Database handle not initialized.");
+    }
+
+    ET_ReturnCode rc = eLoad(pDb);
+    if (rc != H_NO_ERROR)
+    {
+        throw CException(H_ERROR_GENERAL, L"Unable to initalize parsing tree.");
+    }
+}
+
+CParsingTree::~CParsingTree()
+{
+    eTraverseAndDelete(m_pRoot);
+}
+
+static int s_iLevel;
+void CParsingTree::eSplit(const CEString& sWord)
+{
+    s_iLevel = 0;
+    m_vecMatches.clear();
+    Traverse(m_pRoot, sWord);
+}
+
+void CParsingTree::Traverse(StNode * pNode, const CEString& sWord)
+{
+    //    CEString sMsg(L" === ");
+    //    for (int i = 0; i < s_iLevel; ++i)
+    //    {
+    //        sMsg += L'\t';
+    //    }
+    //    sMsg += pRoot->cLetter;
+    //    CLogger::bWriteLog(wstring(sMsg));
+
+    if (s_iLevel > 0 && sWord[sWord.uiLength() - s_iLevel] != pNode->cLetter)
+    {
+        return;
+    }
+
+    m_vecMatches.push_back(s_iLevel);
+
+    ++s_iLevel;
+    vector<StNode *>::iterator it_ = pNode->vecChildren.begin();
+    for (; it_ != pNode->vecChildren.end(); ++it_)
+    {
+        Traverse(*it_, sWord);
+    }
+    --s_iLevel;
+}
+
+ET_ReturnCode CParsingTree::eGetFirstMatch(int& iEndingLength)
+{
+    m_itCurrentMatch = m_vecMatches.begin();
+    if (m_vecMatches.end() == m_itCurrentMatch)
+    {
+        return H_FALSE;
+    }
+
+    iEndingLength = *m_itCurrentMatch;
+
+    return H_NO_ERROR;
+}
+
+ET_ReturnCode CParsingTree::eGetNextMatch(int& iEndingLength)
+{
+    if (m_vecMatches.end() == ++m_itCurrentMatch)
+    {
+        return H_FALSE;
+    }
+
+    iEndingLength = *m_itCurrentMatch;
+
+    return H_NO_ERROR;
+}
+
+ET_ReturnCode CParsingTree::eIsEmptyEnding(__int64 llEndingId)
+{
+    if (m_setEmptyEndingIds.empty())
+    {
+        return H_ERROR_UNEXPECTED;
+    }
+
+    if (m_setEmptyEndingIds.end() == m_setEmptyEndingIds.find(llEndingId))
+    {
+        return H_FALSE;
+    }
+    else
+    {
+        return H_TRUE;
+    }
+}
+
+ET_ReturnCode CParsingTree::eLoad(CSqlite * pDb)
+{
+    // ся/сь!!!!!!
+    // common dev????
+
+    if (NULL == pDb)
+    {
+        return H_ERROR_POINTER;
+    }
+
+    m_mapSortedEndingsList.clear();
+    m_setEmptyEndingIds.clear();
+    m_setEndingStrings.clear();
+
+    try
+    {
+        pDb->PrepareForSelect(sSelectBase);
+        while (pDb->bGetRow())
+        {
+            CEString sEnding;
+            __int64 llDbKey = 0;
+            pDb->GetData(0, (__int64)llDbKey);
+            pDb->GetData(1, sEnding);
+            m_mapSortedEndingsList[sEnding].push_back(llDbKey);
+            m_setEndingStrings.insert(sEnding);
+        }
+    }
+    catch (...)
+    {
+        CEString sMsg;
+        try
+        {
+            CEString sError;
+            pDb->GetLastError(sError);
+            sMsg = L"DB error: ";
+            sMsg += sError;
+            sMsg += L"; code = ";
+            sMsg += CEString::sToString(pDb->iGetLastError());
+        }
+        catch (...)
+        {
+            sMsg = L"Apparent DB error ";
+        }
+
+        sMsg += CEString::sToString(pDb->iGetLastError());
+        ERROR_LOG(sMsg);
+        return H_ERROR_DB;
+    }
+
+    MapEndingToDescriptors::iterator itEnding = m_mapSortedEndingsList.begin();
+
+    // TEST ONLY
+    //    for (; itEnding != m_mapSortedEndingsList.end(); ++itEnding)
+    //    {
+    //        const CEString& sEnding = (*itEnding).first;
+    //        CLogger::bWriteLog(wstring(sEnding));
+    //    }
+    //    itEnding = m_mapSortedEndingsList.begin();
+    // END TEST ONLY
+
+    /*
+    wchar_t cLastChar = '\0';
+    for (; itEnding != m_mapSortedEndingsList.end(); ++itEnding)
+    {
+    const CEString& sEnding = (*itEnding).first;
+    if (sEnding[sEnding.uiLength - 1] != cLastChar)
+    {
+    StNode * pNode = new StNode;
+    }
+    }
+    */
+
+    m_pRoot = new StNode;
+    m_pRoot->cLetter = L'\0';
+    m_pRoot->pPrevious = NULL;
+
+    while ((*itEnding).first.uiLength() < 1)
+    {
+        vector<__int64>::iterator itEndingDataKey = (*itEnding).second.begin();
+        for (; itEndingDataKey != (*itEnding).second.end(); ++itEndingDataKey)
+        {
+            m_setEmptyEndingIds.insert(*itEndingDataKey);
+        }
+        ++itEnding;
+    }
+
+    vector<CEString> vecAll;
+    while (itEnding != m_mapSortedEndingsList.end())
+    {
+        vecAll.push_back((*itEnding++).first);
+    }
+
+    AddLevel(0, m_pRoot, vecAll);
+
+    return H_NO_ERROR;
+
+}       //  ET_ReturnCode CParsingTree::Load(CSqlite * pDb)
+
+void CParsingTree::AddLevel(unsigned int uiOffset, StNode * pParent, vector<CEString> vecInBucket)
+{
+    wchar_t chrCurrent = L'\0';
+    StNode * pCurrentNode = NULL;
+    vector<CEString> vecOutBucket;
+
+    vector<CEString>::iterator itEnding = vecInBucket.begin();
+    for (; itEnding != vecInBucket.end(); ++itEnding)
+    {
+        CEString sNext = *itEnding;
+        if (sNext.uiLength() - uiOffset <= 0)
+        {
+            return; // ERROR?
+        }
+
+        wchar_t chrNext = sNext[sNext.uiLength() - uiOffset - 1];
+        if (chrNext != chrCurrent)
+        {
+            if (!vecOutBucket.empty())
+            {
+                AddLevel(uiOffset + 1, pCurrentNode, vecOutBucket);
+                vecOutBucket.clear();
+            }
+
+            pCurrentNode = new StNode;
+            pCurrentNode->pPrevious = pParent;
+            pCurrentNode->cLetter = chrNext;
+            pParent->vecChildren.push_back(pCurrentNode);
+            chrCurrent = chrNext;
+        }
+
+        if ((int)sNext.uiLength() - (int)uiOffset - 2 >= 0)
+        {
+            vecOutBucket.push_back(*itEnding);
+        }
+    }   // for...
+
+    if (!vecOutBucket.empty())
+    {
+        AddLevel(uiOffset + 1, pCurrentNode, vecOutBucket);
+        vecOutBucket.clear();
+    }
+
+}       //  AddLevel()
+
+ET_ReturnCode CParsingTree::eTraverseAndDelete(StNode * pRoot)
+{
+    if (NULL == pRoot)
+    {
+        ERROR_LOG(L"Root node does not exit.");
+    }
+
+    vector<StNode *>::iterator it_ = pRoot->vecChildren.begin();
+    for (; it_ != pRoot->vecChildren.end(); ++it_)
+    {
+        eTraverseAndDelete(*it_);
+    }
+    delete pRoot;
+
+    return H_NO_ERROR;
+}
+
---
+++ MainLib/ParsingTree.h
@@ -0,0 +1,118 @@
+#ifndef PARSINGTREE_H_INCLUDED
+#define PARSINGTREE_H_INCLUDED
+
+using namespace std;
+
+//
+// Endings parse tree
+//
+namespace Hlib
+{
+    //
+    // Possible wordform division
+    //
+    struct StSplit
+    {
+        unsigned int uiEndingLength;
+        vector<__int64> vecEndingKeys;
+    };
+
+    //
+    // Endings parser node
+    //
+    struct StNode
+    {
+        wchar_t cLetter;
+        StNode * pPrevious;
+        vector<StNode *> vecChildren;
+    };
+
+    struct StReverseComparisonFunctor
+    {
+        bool operator() (const CEString& sLeft, const CEString& sRight)
+        {
+            int iLeftLength = sLeft.uiLength();
+            int iRightLength = sRight.uiLength();
+            if (iLeftLength < 1 && iRightLength < 1)
+            {
+                return false;
+            }
+            if (iLeftLength < 1)
+            {
+                return true;
+            }
+            if (iRightLength < 1)
+            {
+                return false;
+            }
+
+            if (iLeftLength > 10 || iRightLength > 10)
+            {
+                throw CException(H_ERROR_INVALID_ARG, L"Ending too long.");
+            }
+
+            int iLeftOffset = iLeftLength - 1;
+            int iRightOffset = iRightLength - 1;
+            for (; iLeftOffset >= 0 && iRightOffset >= 0; --iLeftOffset, --iRightOffset)
+            {
+                if (sLeft[iLeftOffset] < sRight[iRightOffset])
+                {
+                    return true;
+                }
+                else if (sLeft[iLeftOffset] > sRight[iRightOffset])
+                {
+                    return false;
+                }
+            }
+
+            if (iLeftOffset >= 0)
+            {
+                return false;
+            }
+            else if (iRightOffset >= 0)
+            {
+                return true;;
+            }
+
+            return false;
+
+        }   //  bool operator() (const CEString& sLeft, const CEString& sRight)
+
+    };  //  struct StReverseComparisonFunctor
+
+    class CParsingTree
+    {
+    public:
+        CParsingTree(CSqlite * pDb);
+        ~CParsingTree();
+
+    public:
+        void eSplit(const CEString& sWord);
+        ET_ReturnCode eGetFirstMatch(int& iEndingLength);
+        ET_ReturnCode eGetNextMatch(int& iEndingLength);
+        ET_ReturnCode eIsEmptyEnding(__int64 llEndingId);
+
+    private:
+        typedef map<CEString, vector<__int64>, StReverseComparisonFunctor> MapEndingToDescriptors;
+
+        MapEndingToDescriptors m_mapSortedEndingsList;
+        StNode * m_pRoot;
+        vector<int> m_vecMatches;    // length of ending
+        vector<int>::iterator m_itCurrentMatch;
+        set<CEString, StReverseComparisonFunctor> m_setEndingStrings;
+        set<__int64> m_setEmptyEndingIds;
+
+    protected:
+        CParsingTree() {};
+        ET_ReturnCode eLoad(CSqlite * pDb);
+        void CParsingTree::AddLevel(unsigned int uiOffset, StNode * pParent, vector<CEString> vecEndings);
+        void Traverse(StNode * pRoot, const CEString& sWord);
+        ET_ReturnCode eTraverseAndDelete(StNode * pRoot);
+
+    private:
+
+    };      // CParsingTree
+
+} // namespace Hlib
+
+#endif // PARSINGTREE_H_INCLUDED
