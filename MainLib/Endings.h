c4d28bd2b6a039047f32122b8a32b76e581920f8
Konstantin Bogatyrev <kbogatyrev@gopro.com>
2015-01-24 20:47:42 -0800

    Progress checkin.

--- MainLib/Endings.h
+++ MainLib/Endings.h
@@ -25,66 +25,26 @@ public:
 
     virtual void Reset()
     {
-        m_mmEndings.clear();
+        m_vecEndings.clear();
     }
 
     int iCount()
     {
-//        unsigned __int64 ullH = ullHash();
-//        return m_mmEndings.count (ullH);
         return m_vecEndings.size();
     }
 
-    virtual ET_ReturnCode eLoad() = 0;
-    virtual ET_ReturnCode eAddEnding() = 0;
-
-    // Set ending data for inflection category
-    ET_ReturnCode Select(ET_Number, ET_Case, ET_StressLocation);                                            // nouns    
-    ET_ReturnCode Select(ET_Subparadigm, ET_Gender, ET_Number, ET_Case, ET_Animacy);                     // long adjectives
-    void Select(ET_Gender, ET_Number, ET_StressLocation);                                          // short adjectives
-    ET_ReturnCode Select(ET_Person, ET_Number, ET_StressLocation, ET_StemAuslaut, int iInflectionType, ET_Reflexive);     // personal endings
-    void Select(int iInflectionType);                                                              // infinitive
-    void Select(ET_Gender, ET_Number, ET_StemAuslaut);                                             // past tense
+//    virtual ET_ReturnCode eLoad() = 0;
+//    virtual ET_ReturnCode eAddEnding() = 0;
 
     ET_ReturnCode eGetEnding(int iSeqNum, CEString& sEnding, unsigned __int64& llEndingKey);
-
-/*
-    ET_ReturnCode eGetEnding (int iSeqNum, CEString& sEnding, unsigned __int64& llEndingKey)
-    {
-        unsigned __int64 llKey = ullHash();
-
-        ItPair pair_Range = m_mmEndings.equal_range (llKey);
-        for (int iItem = 0; iItem < iSeqNum; ++iItem)
-        {
-            if (++pair_Range.first == pair_Range.second)
-            {
-                CEString sMsg (L"Error getting ending from hash.");
-                ERROR_LOG (sMsg);
-                return H_ERROR_GENERAL;
-            }
-        }
-
-        pair<CEString, unsigned __int64> pairEndingData = (*pair_Range.first).second;
-        sEnding = pairEndingData.first;
-        llEndingKey = pairEndingData.second;
-
-        return H_NO_ERROR;
-    }
-*/
-
     ET_ReturnCode eGetEnding (CEString& s_, unsigned __int64& llKey)
     {
         return eGetEnding (0, s_, llKey);
     }
 
 protected:
-    virtual unsigned __int64 ullHash() const = 0;
     void ReportDbError();
-    std::multimap<unsigned __int64, pair<CEString, unsigned __int64> > m_mmEndings;       // ending hash (64-bit) to ending data key
     std::vector<pair<unsigned __int64, CEString> > m_vecEndings;
-
-    typedef std::multimap<unsigned __int64, pair<CEString, unsigned __int64> > EndingsMultiMap;
-    typedef pair<EndingsMultiMap::const_iterator, EndingsMultiMap::const_iterator> ItPair;
 
     CLexeme * m_pLexeme;
     unsigned __int64 m_ullDbKey;
@@ -106,11 +66,14 @@ class CNounEndings : public CEndings
 public:
     CNounEndings(CLexeme *);
 
-    virtual ET_ReturnCode eLoad();
-    virtual ET_ReturnCode eAddEnding();
+//    virtual ET_ReturnCode eLoad();
+//    virtual ET_ReturnCode eAddEnding();
+
+    ET_ReturnCode Select(ET_Number, ET_Case, ET_StressLocation);                                            // nouns    
+
 
 private:
-    virtual unsigned __int64 ullHash() const;
+//    virtual unsigned __int64 ullHash() const;
 
 };      //  CNounEndings
 
@@ -119,11 +82,13 @@ class CAdjLongEndings : public CEndings
 public:
     CAdjLongEndings(CLexeme *, ET_Subparadigm);
 
-    virtual ET_ReturnCode eLoad();
-    virtual ET_ReturnCode eAddEnding();
+//    virtual ET_ReturnCode eLoad();
+//    virtual ET_ReturnCode eAddEnding();
+    ET_ReturnCode eSelect(ET_Subparadigm, ET_Gender, ET_Number, ET_Case, ET_Animacy);                     // long adjectives
+
 
 private:
-    virtual unsigned __int64 ullHash() const;
+//    virtual unsigned __int64 ullHash() const;
 
     ET_Subparadigm m_eSubparadigm;
 
@@ -134,11 +99,14 @@ class CAdjShortEndings : public CEndings
 public:
     CAdjShortEndings(CLexeme * pLexeme);
 
-    virtual ET_ReturnCode eLoad();
-    virtual ET_ReturnCode eAddEnding();
+//    virtual ET_ReturnCode eLoad();
+//    virtual ET_ReturnCode eAddEnding();
+
+    ET_ReturnCode eSelect(ET_Gender, ET_Number, ET_StressLocation);                                          // short adjectives
+
 
 private:
-    virtual unsigned __int64 ullHash() const;
+//    virtual unsigned __int64 ullHash() const;
 
 };    //  CAdjShortEndings
 
@@ -147,11 +115,13 @@ class CPersonalEndings : public CEndings
 public:
     CPersonalEndings(CLexeme * pLexeme);
 
-    virtual ET_ReturnCode eLoad();
-    virtual ET_ReturnCode eAddEnding();
+//    virtual ET_ReturnCode eLoad();
+//    virtual ET_ReturnCode eAddEnding();
+    ET_ReturnCode Select(ET_Person, ET_Number, ET_StressLocation, ET_StemAuslaut, int iInflectionType, ET_Reflexive);     // personal endings
+
 
 private:
-    virtual unsigned __int64 ullHash() const;
+//    virtual unsigned __int64 ullHash() const;
 
 };   //  CPersonalEndings
 
@@ -160,11 +130,14 @@ class CInfinitiveEndings : public CEndings
 public:
     CInfinitiveEndings(CLexeme * pLexeme);
 
-    virtual ET_ReturnCode eLoad();
-    virtual ET_ReturnCode eAddEnding();
+//    virtual ET_ReturnCode eLoad();
+//    virtual ET_ReturnCode eAddEnding();
+
+    void Select(int iInflectionType);                                                              // infinitive
+
 
 private:
-    virtual unsigned __int64 ullHash() const;
+//    virtual unsigned __int64 ullHash() const;
 
 };    //   CInfinitiveEndings
 
@@ -173,11 +146,15 @@ class CPastTenseEndings : public CEndings
 public:
     CPastTenseEndings(CLexeme * pLexeme);
 
-    virtual ET_ReturnCode eLoad();
-    virtual ET_ReturnCode eAddEnding();
+//    virtual ET_ReturnCode eLoad();
+//    virtual ET_ReturnCode eAddEnding();
+
+    void Select(ET_Gender, ET_Number, ET_StemAuslaut);                                             // past tense
+
+
 
 private:
-    virtual unsigned __int64 ullHash() const;
+//    virtual unsigned __int64 ullHash() const;
 };    //  CPastTenseEndings
 
 } // namespace Hlib
