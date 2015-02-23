c3f5a5f5234fb08e9520af15362ab6c0d0c7f702
Konstantin Bogatyrev <kbogatyrev@gopro.com>
2015-02-23 12:21:39 -0800

    Initial revision.

---
+++ TestUI/RegressionControl.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace TestUI
+{
+    partial class GridViewUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.checkBoxSelectAll = new System.Windows.Forms.CheckBox();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.Test = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.LexemeId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Source = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Test,
+            this.LexemeId,
+            this.Source,
+            this.Status});
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersVisible = false;
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            this.dataGridView.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.ShowEditingIcon = false;
+            this.dataGridView.Size = new System.Drawing.Size(590, 748);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Location = new System.Drawing.Point(616, 153);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.Enabled = false;
+            this.buttonCancel.Location = new System.Drawing.Point(616, 213);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.Location = new System.Drawing.Point(616, 243);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 4;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRun.Location = new System.Drawing.Point(616, 123);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 23);
+            this.buttonRun.TabIndex = 1;
+            this.buttonRun.Text = "Start";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // checkBoxSelectAll
+            // 
+            this.checkBoxSelectAll.AutoSize = true;
+            this.checkBoxSelectAll.Location = new System.Drawing.Point(5, 4);
+            this.checkBoxSelectAll.Name = "checkBoxSelectAll";
+            this.checkBoxSelectAll.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxSelectAll.TabIndex = 5;
+            this.checkBoxSelectAll.UseVisualStyleBackColor = true;
+            this.checkBoxSelectAll.CheckedChanged += new System.EventHandler(this.checkBoxSelectAll_CheckedChanged);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDelete.Location = new System.Drawing.Point(616, 183);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 6;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // Test
+            // 
+            this.Test.HeaderText = "";
+            this.Test.Name = "Test";
+            this.Test.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Test.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Test.Width = 20;
+            // 
+            // LexemeId
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.NullValue = null;
+            this.LexemeId.DefaultCellStyle = dataGridViewCellStyle1;
+            this.LexemeId.HeaderText = "ID";
+            this.LexemeId.MinimumWidth = 50;
+            this.LexemeId.Name = "LexemeId";
+            this.LexemeId.ReadOnly = true;
+            this.LexemeId.Width = 300;
+            // 
+            // Source
+            // 
+            this.Source.HeaderText = "Initial Form";
+            this.Source.Name = "Source";
+            this.Source.ReadOnly = true;
+            this.Source.Width = 150;
+            // 
+            // Status
+            // 
+            this.Status.HeaderText = "Status";
+            this.Status.Name = "Status";
+            this.Status.ReadOnly = true;
+            // 
+            // GridViewUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.checkBoxSelectAll);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "GridViewUserControl";
+            this.Size = new System.Drawing.Size(720, 748);
+            this.VisibleChanged += new System.EventHandler(this.GridViewUserControl_VisibleChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.CheckBox checkBoxSelectAll;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Test;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LexemeId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Source;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+    }
+}
---
+++ TestUI/RegressionControl.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+using MainLibManaged;
+
+namespace TestUI
+{
+    public partial class GridViewUserControl : UserControl
+    {
+        Thread m_WorkerThread;
+        public string m_sDbPath;
+        public bool m_bCancelTest = false;
+
+        public TestApplet m_Parent
+        {
+            get;
+            set;
+        }
+
+        public GridViewUserControl(string sDbPath)
+        {
+            InitializeComponent();
+            m_sDbPath = sDbPath;
+            checkBoxSelectAll.Checked = true;
+        }
+
+        public int iRows
+        {
+            get
+            {
+                return dataGridView.RowCount;
+            }
+        }
+
+        public void SetResult (int iRow, string sText, bool bHightLight)
+        {
+            DataGridViewCell cell = dataGridView[3, iRow];
+            cell.Value = sText;
+            if (bHightLight)
+            {
+                cell.Style.ForeColor = Color.Red;
+            }
+        }
+
+        public bool bRowChecked(int iRow)
+        {
+            DataGridViewCell cell = dataGridView[0, iRow];
+            DataGridViewCheckBoxCell cbCheckBox = (DataGridViewCheckBoxCell)cell;
+            return (bool)cbCheckBox.Value;
+        }
+
+        public string sLexemeHash (int iRow)
+        {
+            DataGridViewCell cell = dataGridView[1, iRow];
+            DataGridViewTextBoxCell textBoxCell = (DataGridViewTextBoxCell)cell;
+            return (string)textBoxCell.Value;
+        }
+
+/*
+        public MainLib.IVerifier Verifier(int iRow)
+        {
+            DataGridViewCell cell = dataGridView[1, iRow];
+            return (MainLib.IVerifier)cell.Tag;
+        }
+*/
+        public string sHeadword(int iRow)
+        {
+            DataGridViewCell cell = dataGridView[2, iRow];
+            DataGridViewTextBoxCell textBoxCell = (DataGridViewTextBoxCell)cell;
+            return (string)textBoxCell.Value;
+        }
+
+        public void Clear()
+        {
+            dataGridView.Rows.Clear();
+        }
+
+        public void AddLexeme (string sHeadword, string sHash)
+        {
+            DataGridViewRow r = new DataGridViewRow();
+            r.CreateCells(dataGridView);
+            r.Cells[0].Value = true;
+            r.Cells[1].Value = sHash;
+//            r.Cells[1].Tag = v;
+            r.Cells[2].Value = sHeadword;
+            dataGridView.Rows.Add(r);
+        }
+
+        private void GridViewUserControl_VisibleChanged (object sender, EventArgs e)
+        {
+            this.Size = Parent.Size;
+            dataGridView.Width = buttonRun.Location.X - (Width - buttonRun.Width - buttonRun.Location.X);
+            buttonRun.Focus();
+        }
+
+        private void buttonRun_Click (object sender, EventArgs e)
+        {
+            buttonRun.Enabled = false;
+            buttonCancel.Enabled = true;
+
+            foreach (DataGridViewRow row in dataGridView.Rows)
+            {
+                DataGridViewCheckBoxCell cbCheckBox = (DataGridViewCheckBoxCell)row.Cells[0];
+                if (false == (bool)cbCheckBox.Value)
+                {
+                    continue;
+                }
+
+                DataGridViewCell cbStatus = row.Cells[3];
+                cbStatus.Value = "Testing...";
+            }
+
+            m_Parent.Verify(this);
+
+        }   // buttonRun_Click (...)
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            TestApplet ta = (TestApplet)Parent.Parent.Parent;
+            //ta.SaveTestResults();
+
+            ((Button)sender).Enabled = false;
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            List<DataGridViewRow> rowsToRemove = new List<DataGridViewRow>();
+            foreach (DataGridViewRow r in dataGridView.Rows)
+            {
+                if (!bRowChecked(r.Index))
+                {
+                    continue;
+                }
+
+                try
+                {
+//                    MainLib.ZalStoredLexemeData testData = new MainLib.ZalStoredLexemeData();
+//                    testData.DbPath = m_sDbPath;
+//                    testData.DeleteStoredLexeme(sLexemeHash(r.Index));
+                    m_Parent.m_Verifier.eDeleteStoredLexeme(sLexemeHash(r.Index));
+                    rowsToRemove.Add(r);
+                }
+                catch (Exception ex)
+                {
+//                    MainLib.ZalError err = new MainLib.ZalError();
+                    string sMsg = ex.Message;
+                    sMsg += "\n";
+//                    sMsg += err.LastError;
+                    MessageBox.Show (sMsg, "Error", MessageBoxButtons.OK);
+                    return;
+                }
+            }   // foreach ...
+
+            foreach (DataGridViewRow r in rowsToRemove)
+            {
+                dataGridView.Rows.Remove(r);
+            }
+            dataGridView.Update();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            m_bCancelTest = true;
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            TestApplet ta = (TestApplet)Parent.Parent.Parent;
+            ta.CloseCurrentTab();
+        }
+
+        private void checkBoxSelectAll_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView.Rows)
+            {
+                DataGridViewCheckBoxCell cbCheckBox = (DataGridViewCheckBoxCell)row.Cells[0];
+                cbCheckBox.Value = checkBoxSelectAll.Checked;
+                buttonRun.Enabled = checkBoxSelectAll.Checked;
+            }
+        }
+
+        private void dataGridView_CellValueChanged (object sender, DataGridViewCellEventArgs e)
+        {
+            if (0 == e.ColumnIndex)
+            {
+                if (true == (bool)dataGridView[e.ColumnIndex, e.RowIndex].Value)
+                {
+                    buttonRun.Enabled = true;
+                }
+            }
+        }
+
+        private void dataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (0 == e.ColumnIndex)
+            {
+                if (true == (bool)dataGridView[e.ColumnIndex, e.RowIndex].Value)
+                {
+                    buttonRun.Enabled = true;
+                }
+            }
+        }
+
+        private void dataGridView_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            if (0 == e.ColumnIndex)
+            {
+                if (true == (bool)dataGridView[e.ColumnIndex, e.RowIndex].Value)
+                {
+                    buttonRun.Enabled = true;
+                }
+            }
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (0 == e.ColumnIndex)
+            {
+                if (true == (bool)dataGridView[e.ColumnIndex, e.RowIndex].EditedFormattedValue)
+                {
+                    buttonRun.Enabled = true;
+                }
+                else
+                {
+                    foreach (DataGridViewRow row in dataGridView.Rows)
+                    {
+                        buttonRun.Enabled = false;
+                        DataGridViewCell cell = row.Cells[0];
+                        if ((bool)cell.EditedFormattedValue)
+                        {
+                            buttonRun.Enabled = true;
+                        }
+                    }
+                }
+            }
+        }
+    }       //  public partial class GridViewUserControl
+
+}  //  namespace TestUI
