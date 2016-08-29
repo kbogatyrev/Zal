d20045f14c0eb2c89ff11e2bd2d4afe29869d410
Konstantin Bogatyrev <kbogatyrev@gopro.com>
2016-08-29 14:15:21 -0800

    Bug fix.

--- MainLibManaged/MainLibManaged.cpp
+++ MainLibManaged/MainLibManaged.cpp
@@ -14,10 +14,13 @@ using namespace MainLibManaged;
 CEString sFromManagedString(String^ sSource)
 {
     // This conversion requires an instance of marshal_context class
-    marshal_context^ mc = gcnew marshal_context();
-    CEString cestrResult(mc->marshal_as<const wchar_t *>(sSource));
-    delete mc;
+    //marshal_context^ mc = gcnew marshal_context();
+    //CEString cestrResult(mc->marshal_as<const wchar_t *>(sSource));
 
+
+    // Pin memory so GC can't move it while native function is called
+    pin_ptr<const wchar_t> pSource = PtrToStringChars(sSource);
+    CEString cestrResult(pSource);
     return cestrResult;
 }
 
