0ab9a79025e64cbeaef3aaf2e25cb130d9063855
Konstantin Bogatyrev <kbogatyrev@yahoo.com>
2017-06-29 15:32:06 -0700

    Initial revision.

---
+++ TestApp/NounViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace ZalTestApp
+{
+    public class NounViewModel : ViewModelBase
+    {
+        public NounViewModel()
+        {
+            int kiki = 0;
+        }
+
+        private string m_sSourceForm = "псевдосинхрофазотрон";
+        public string SourceForm
+        {
+            get
+            {
+                return m_sSourceForm;
+            }
+            set
+            {
+                m_sSourceForm = value;
+                OnPropertyChanged("SourceForm");
+            }
+        }
+        private string m_Noun_Sg_N = "моча\x301лка";
+        public string Noun_Sg_N
+        {
+            get
+            {
+                return m_Noun_Sg_N;
+            }
+            set
+            {
+                m_sSourceForm = value;
+                OnPropertyChanged("Noun_Sg_N");
+            }
+        }
+    }
+}
