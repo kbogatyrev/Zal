aa83bc1863499f909fedc77b645d2a2ede8154dd
Konstantin <kbogatyrev@yahoo.com>
2017-07-10 10:18:47 -0700

    Initial revision.

---
+++ TestApp/LexemeGridViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MainLibManaged;
+using System.Collections.ObjectModel;
+
+namespace ZalTestApp
+{
+    public class LexemeGridViewModel : ViewModelBase
+    {
+        private ObservableCollection<LexemeViewModel> m_Lexemes;
+        public ObservableCollection<LexemeViewModel> LexemeCollection
+        {
+//            set
+//            {
+//                m_Lexemes = value;
+//            }
+            get
+            {
+                return m_Lexemes;
+            }
+        }
+
+        public LexemeGridViewModel()
+        {
+            m_Lexemes = new ObservableCollection<LexemeViewModel>();
+        }
+
+        public void Add (LexemeViewModel lvm)
+        {
+            m_Lexemes.Add(lvm);
+        }
+    }
+}
