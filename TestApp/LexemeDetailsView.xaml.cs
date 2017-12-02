06a506cb23a65438588795d32eb9109e5ed8a80c
Konstantin <kbogatyrev@yahoo.com>
2017-12-02 06:41:23 -0800

    Remove module.

---
+++ TestApp/LexemeDetailsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ZalTestApp
+{
+    /// <summary>
+    /// Interaction logic for LexemeDetails.xaml
+    /// </summary>
+    public partial class LexemeDetailsView : UserControl
+    {
+        public LexemeDetailsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
