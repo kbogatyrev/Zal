2543d676a6741c127081ca142844c5b3af6d87de
Konstantin <kbogatyrev@yahoo.com>
2018-01-28 10:14:34 -0800

    Issue #106: Варианты не учитываются при порождении форм.

--- MainLib/FormBuilderVerbImpv.h
+++ MainLib/FormBuilderVerbImpv.h
@@ -19,24 +19,24 @@ public:
     {}
 
 public:
-    ET_ReturnCode eGetLemma (CEString& sLemma);
-    ET_ReturnCode eGetEndingType (const CEString& sLemma, int& iType);
+    ET_ReturnCode eGetStem(CEString&);
+    ET_ReturnCode eGetEndingType (const CEString& sStem, int& iType);
                             // Ending type is an integer NOT defined in GD but rather assigned in the endings source file
                             // based on various in order to simplify DB structure. May need to be refactored.
-    ET_ReturnCode eGetStressPositions (const CEString& sLemma, 
+    ET_ReturnCode eGetStressPositions (const CEString& sStem, 
                                        const CEString& sEnding,
                                        vector<int>& vecStressPositions);
-    ET_ReturnCode eCreateFormTemplate (const CEString& sLemma,
+    ET_ReturnCode eCreateFormTemplate (const CEString& sStem,
                                        CEString& sEnding,
                                        __int64 llEndingDataId,
                                        ET_Number eNumber,
                                        CWordForm *& pWordForm);
-    ET_ReturnCode eHandleCommonDeviations (const CEString& sLemma, int& iVariantEndingType);
+    ET_ReturnCode eHandleCommonDeviations (const CEString& sStem, int& iVariantEndingType);
     ET_ReturnCode eCheckForIrregularForms (bool& bIsVariant); // check if irregular forms were built and
                                                         // regular processing must be skipped
 
     ET_ReturnCode eBuild();
-    ET_ReturnCode eAssembleWordForm(ET_Number eNumber, const CEString& sLemma, int iEndingType, CWordForm *& pWordForm);
+    ET_ReturnCode eAssembleWordForm(ET_Number eNumber, const CEString& sStem, int iEndingType, CWordForm *& pWordForm);
     ET_ReturnCode eBuildIrregularForms();
 
 protected:
