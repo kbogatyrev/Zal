39e60e46ea37b75846dde469f2d6206c2a61c591
Konstantin <kbogatyrev@yahoo.com>
2018-02-21 07:28:30 -0800

    Initial revision.

---
+++ TestApp/EnterLexemePropertiesDlg.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+using MainLibManaged;
+
+namespace ZalTestApp
+{
+    /// <summary>
+    /// Interaction logic for EnterLexemeProperties.xaml
+    /// </summary>
+    public partial class EnterLexemePropertiesDlg : Window
+    {
+        public EnterLexemePropertiesDlg(EnterLexemePropertiesViewModel clvm)
+        {
+            InitializeComponent();
+            this.DataContext = clvm;
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            ((EnterLexemePropertiesViewModel)this.DataContext).OnCancel(null);
+            this.Close();
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            ((EnterLexemePropertiesViewModel)this.DataContext).OnOK(null);
+            this.Close();
+        }
+    }
+}
