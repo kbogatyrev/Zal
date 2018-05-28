6ce68b6eb892833f22543120ad346f734ffd7be2
Konstantin <kbogatyrev@yahoo.com>
2018-05-28 14:40:27 -0700

    Enable form editing (progress checkin).

--- TestApp/NounViewModel.cs
+++ TestApp/NounViewModel.cs
@@ -3,6 +3,8 @@ using System.Windows.Input;
 using System.Collections.Generic;
 
 using MainLibManaged;
+using System.Windows;
+using System.ComponentModel;
 
 namespace ZalTestApp
 {
@@ -11,9 +13,13 @@ namespace ZalTestApp
         public delegate void BackButtonHandler();
         public event BackButtonHandler BackButtonEvent;
 
+        public delegate void EditButtonHandler();
+//        public event EditButtonHandler EditButtonEvent;
+
         MainModel m_MainModel = null;
 
         #region ICommand
+
         private ICommand m_BackCommand;
         public ICommand BackCommand
         {
@@ -26,9 +32,37 @@ namespace ZalTestApp
                 m_BackCommand = value;
             }
         }
+
+        private ICommand m_EditCommand;
+        public ICommand EditCommand
+        {
+            get
+            {
+                return m_EditCommand;
+            }
+            set
+            {
+                m_EditCommand = value;
+            }
+        }
+
         #endregion
 
         #region Bindings
+
+        private bool m_bReadOnly;
+        public bool ReadOnly
+        {
+            get
+            {
+                return m_bReadOnly;
+            }
+            set
+            {
+                m_bReadOnly = value;
+                OnPropertyChanged("ReadOnly");
+            }
+        }
 
         private string m_sSourceForm;
         public string SourceForm
@@ -58,6 +92,20 @@ namespace ZalTestApp
             }
         }
 
+        private bool m_bNoun_Sg_N_IsReadOnly = true;
+        public bool Noun_Sg_N_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Sg_N_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Sg_N_IsReadOnly = value;
+                OnPropertyChanged("Noun_Sg_N_IsReadOnly");
+            }
+        }
+
         private string m_sNoun_Sg_A;
         public string Noun_Sg_A
         {
@@ -69,6 +117,20 @@ namespace ZalTestApp
             {
                 m_sNoun_Sg_A = value;
                 OnPropertyChanged("Noun_Sg_A");
+            }
+        }
+
+        private bool m_bNoun_Sg_A_IsReadOnly = true;
+        public bool Noun_Sg_A_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Sg_A_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Sg_A_IsReadOnly = value;
+                OnPropertyChanged("Noun_Sg_A_IsReadOnly");
             }
         }
 
@@ -86,6 +148,20 @@ namespace ZalTestApp
             }
         }
 
+        private bool m_bNoun_Sg_G_IsReadOnly = true;
+        public bool Noun_Sg_G_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Sg_G_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Sg_G_IsReadOnly = value;
+                OnPropertyChanged("Noun_Sg_G_IsReadOnly");
+            }
+        }
+
         private string m_sNoun_Sg_P;
         public string Noun_Sg_P
         {
@@ -97,6 +173,20 @@ namespace ZalTestApp
             {
                 m_sNoun_Sg_P = value;
                 OnPropertyChanged("Noun_Sg_P");
+            }
+        }
+
+        private bool m_bNoun_Sg_P_IsReadOnly = true;
+        public bool Noun_Sg_P_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Sg_P_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Sg_P_IsReadOnly = value;
+                OnPropertyChanged("Noun_Sg_P_IsReadOnly");
             }
         }
 
@@ -114,6 +204,20 @@ namespace ZalTestApp
             }
         }
 
+        private bool m_bNoun_Sg_D_IsReadOnly = true;
+        public bool Noun_Sg_D_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Sg_D_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Sg_D_IsReadOnly = value;
+                OnPropertyChanged("Noun_Sg_D_IsReadOnly");
+            }
+        }
+
         private string m_sNoun_Sg_I;
         public string Noun_Sg_I
         {
@@ -124,10 +228,24 @@ namespace ZalTestApp
             set
             {
                 m_sNoun_Sg_I = value;
-                OnPropertyChanged("Noun_Sg_D");
+                OnPropertyChanged("Noun_Sg_I");
             }
         }
-        
+
+        private bool m_bNoun_Sg_I_IsReadOnly = true;
+        public bool Noun_Sg_I_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Sg_I_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Sg_I_IsReadOnly = value;
+                OnPropertyChanged("Noun_Sg_I_IsReadOnly");
+            }
+        }
+
         private string m_sNoun_Sg_Part;
         public string Noun_Sg_Part
         {
@@ -139,6 +257,20 @@ namespace ZalTestApp
             {
                 m_sNoun_Sg_Part = value;
                 OnPropertyChanged("Noun_Sg_Part");
+            }
+        }
+
+        private bool m_bNoun_Sg_Part_IsReadOnly = true;
+        public bool Noun_Sg_Part_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Sg_Part_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Sg_Part_IsReadOnly = value;
+                OnPropertyChanged("Noun_Sg_Part_IsReadOnly");
             }
         }
 
@@ -156,6 +288,20 @@ namespace ZalTestApp
             }
         }
 
+        private bool m_bNoun_Sg_L_IsReadOnly = true;
+        public bool Noun_Sg_L_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Sg_L_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Sg_L_IsReadOnly = value;
+                OnPropertyChanged("Noun_Sg_L_IsReadOnly");
+            }
+        }
+
         private bool m_bIs_L2_optional = false;
         public bool Is_L2_optional
         {
@@ -167,6 +313,20 @@ namespace ZalTestApp
             {
                 m_bIs_L2_optional = value;
                 OnPropertyChanged("Is_L2_optional");
+            }
+        }
+
+        private bool m_bIs_L2_optional_IsReadOnly = true;
+        public bool IsL2_optional_IsReadOnly
+        {
+            get
+            {
+                return m_bIs_L2_optional_IsReadOnly;
+            }
+            set
+            {
+                m_bIs_L2_optional_IsReadOnly = value;
+                OnPropertyChanged("Is_L2_optional_IsReadOnly");
             }
         }
 
@@ -184,6 +344,20 @@ namespace ZalTestApp
             }
         }
 
+        private bool m_bNoun_Sg_L2_Prepositions_IsReadOnly = true;
+        public bool Noun_Sg_L2_Prepositions_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Sg_L2_Prepositions_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Sg_L2_Prepositions_IsReadOnly = value;
+                OnPropertyChanged("Noun_Sg_L2_Prepositions_IsReadOnly");
+            }
+        }
+
         private string m_sNoun_Pl_N;
         public string Noun_Pl_N
         {
@@ -195,6 +369,20 @@ namespace ZalTestApp
             {
                 m_sNoun_Pl_N = value;
                 OnPropertyChanged("Noun_Pl_N");
+            }
+        }
+
+        private bool m_bNoun_Pl_N_IsReadOnly = true;
+        public bool Noun_Pl_N_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Pl_N_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Pl_N_IsReadOnly = value;
+                OnPropertyChanged("Noun_Pl_N_IsReadOnly");
             }
         }
 
@@ -212,6 +400,20 @@ namespace ZalTestApp
             }
         }
 
+        private bool m_bNoun_Pl_A_IsReadOnly = true;
+        public bool Noun_Pl_A_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Pl_A_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Pl_A_IsReadOnly = value;
+                OnPropertyChanged("Noun_Pl_A_IsReadOnly");
+            }
+        }
+
         private string m_sNoun_Pl_G;
         public string Noun_Pl_G
         {
@@ -226,6 +428,21 @@ namespace ZalTestApp
             }
         }
 
+        private bool m_bNoun_Pl_G_IsReadOnly = true;
+        public bool Noun_Pl_G_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Pl_G_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Pl_G_IsReadOnly = value;
+                OnPropertyChanged("Noun_Pl_G_IsReadOnly");
+            }
+        }
+
+
         private string m_sNoun_Pl_P;
         public string Noun_Pl_P
         {
@@ -237,6 +454,20 @@ namespace ZalTestApp
             {
                 m_sNoun_Pl_P = value;
                 OnPropertyChanged("Noun_Pl_P");
+            }
+        }
+
+        private bool m_bNoun_Pl_P_IsReadOnly = true;
+        public bool Noun_Pl_P_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Pl_P_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Pl_P_IsReadOnly = value;
+                OnPropertyChanged("Noun_Pl_P_IsReadOnly");
             }
         }
 
@@ -254,6 +485,20 @@ namespace ZalTestApp
             }
         }
 
+        private bool m_bNoun_Pl_D_IsReadOnly = true;
+        public bool Noun_Pl_D_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Pl_D_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Pl_D_IsReadOnly = value;
+                OnPropertyChanged("Noun_Pl_D_IsReadOnly");
+            }
+        }
+
         private string m_sNoun_Pl_I;
         public string Noun_Pl_I
         {
@@ -265,6 +510,20 @@ namespace ZalTestApp
             {
                 m_sNoun_Pl_I = value;
                 OnPropertyChanged("Noun_Pl_D");
+            }
+        }
+
+        private bool m_bNoun_Pl_I_IsReadOnly = true;
+        public bool Noun_Pl_I_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Pl_I_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Pl_I_IsReadOnly = value;
+                OnPropertyChanged("Noun_Pl_I_IsReadOnly");
             }
         }
 
@@ -282,54 +541,148 @@ namespace ZalTestApp
             }
         }
 
+        private bool m_bNoun_Pl_L_IsReadOnly = true;
+        public bool Noun_Pl_L_IsReadOnly
+        {
+            get
+            {
+                return m_bNoun_Pl_L_IsReadOnly;
+            }
+            set
+            {
+                m_bNoun_Pl_L_IsReadOnly = value;
+                OnPropertyChanged("Noun_Pl_L_IsReadOnly");
+            }
+        }
+
+        private bool m_bEditEnabled = false;
+        public bool EditEnabled
+        {
+            get
+            {
+                return m_bEditEnabled;
+            }
+            set
+            {
+                m_bEditEnabled = value;
+                OnPropertyChanged("EditEnabled");
+            }
+        }
+
         #endregion
 
         public NounViewModel(CLexemeManaged lexeme, MainModel m)
         {
             BackCommand = new RelayCommand(new Action<object>(GoBack));
+            EditCommand = new RelayCommand(new Action<object>(EditForm));
+
+//            ReadOnly = true;
+            EditEnabled = true;
+
             m_MainModel = m;
             string sLexemeHash = lexeme.sHash();
 
-            List<string> forms = null;
-            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_N", out forms);
-            Noun_Sg_N = Helpers.sListToCommaSeparatedString(forms);
-            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_A", out forms);
-            Noun_Sg_A = Helpers.sListToCommaSeparatedString(forms);
-            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_G", out forms);
-            Noun_Sg_G = Helpers.sListToCommaSeparatedString(forms);
-            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_P", out forms);
-            Noun_Sg_P = Helpers.sListToCommaSeparatedString(forms);
-            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_D", out forms);
-            Noun_Sg_D = Helpers.sListToCommaSeparatedString(forms);
-            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_I", out forms);
-            Noun_Sg_I = Helpers.sListToCommaSeparatedString(forms);
+            List<string> m_Forms = null;
 
-            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_Part", out forms);
-            Noun_Sg_Part = Helpers.sListToCommaSeparatedString(forms);
-            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_L", out forms);
+            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_N", out m_Forms);
+            Noun_Sg_N = Helpers.sListToCommaSeparatedString(m_Forms);
 
-            Noun_Sg_L = Helpers.sListToCommaSeparatedString(forms);
+            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_A", out m_Forms);
+            Noun_Sg_A = Helpers.sListToCommaSeparatedString(m_Forms);
+
+            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_G", out m_Forms);
+            Noun_Sg_G = Helpers.sListToCommaSeparatedString(m_Forms);
+
+            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_P", out m_Forms);
+            Noun_Sg_P = Helpers.sListToCommaSeparatedString(m_Forms);
+
+            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_D", out m_Forms);
+            Noun_Sg_D = Helpers.sListToCommaSeparatedString(m_Forms);
+
+            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_I", out m_Forms);
+            Noun_Sg_I = Helpers.sListToCommaSeparatedString(m_Forms);
+
+            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_Part", out m_Forms);
+            Noun_Sg_Part = Helpers.sListToCommaSeparatedString(m_Forms);
+
+            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Sg_L", out m_Forms);
+            Noun_Sg_L = Helpers.sListToCommaSeparatedString(m_Forms);
+
             Noun_Sg_L2_Prepositions = m_MainModel.sGetL2Prepositions(lexeme);
 
             Is_L2_optional = m_MainModel.IsL2Optonal(lexeme);
 
-            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Pl_N", out forms);
-            Noun_Pl_N = Helpers.sListToCommaSeparatedString(forms);
-            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Pl_A", out forms);
-            Noun_Pl_A = Helpers.sListToCommaSeparatedString(forms);
-            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Pl_G", out forms);
-            Noun_Pl_G = Helpers.sListToCommaSeparatedString(forms);
-            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Pl_P", out forms);
-            Noun_Pl_P = Helpers.sListToCommaSeparatedString(forms);
-            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Pl_D", out forms);
-            Noun_Pl_D = Helpers.sListToCommaSeparatedString(forms);
-            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Pl_I", out forms);
-            Noun_Pl_I = Helpers.sListToCommaSeparatedString(forms);
+            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Pl_N", out m_Forms);
+            Noun_Pl_N = Helpers.sListToCommaSeparatedString(m_Forms);
+
+            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Pl_A", out m_Forms);
+            Noun_Pl_A = Helpers.sListToCommaSeparatedString(m_Forms);
+
+            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Pl_G", out m_Forms);
+            Noun_Pl_G = Helpers.sListToCommaSeparatedString(m_Forms);
+
+            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Pl_P", out m_Forms);
+            Noun_Pl_P = Helpers.sListToCommaSeparatedString(m_Forms);
+
+            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Pl_D", out m_Forms);
+            Noun_Pl_D = Helpers.sListToCommaSeparatedString(m_Forms);
+
+            m_MainModel.GetFormsByGramHash(sLexemeHash, "Noun_Pl_I", out m_Forms);
+            Noun_Pl_I = Helpers.sListToCommaSeparatedString(m_Forms);
         }
 
         public void GoBack(Object obj)
         {
             BackButtonEvent?.Invoke();
+        }
+
+        public void EditForm(Object obj)
+        {
+//            Noun_Sg_N_IsReadOnly = false;
+
+            var sPropName = obj as string;
+            switch (obj as string)
+            {
+                case "Noun_Sg_N":
+                    Noun_Sg_N_IsReadOnly = !Noun_Sg_N_IsReadOnly;
+                    break;
+                case "Noun_Sg_A":
+                    Noun_Sg_A_IsReadOnly = !Noun_Sg_A_IsReadOnly;
+                    break;
+                case "Noun_Sg_G":
+                    Noun_Sg_G_IsReadOnly = !Noun_Sg_G_IsReadOnly;
+                    break;
+                case "Noun_Sg_P":
+                    Noun_Sg_P_IsReadOnly = !Noun_Sg_P_IsReadOnly;
+                    break;
+                case "Noun_Sg_D":
+                    Noun_Sg_D_IsReadOnly = !Noun_Sg_D_IsReadOnly;
+                    break;
+                case "Noun_Sg_I":
+                    Noun_Sg_I_IsReadOnly = !Noun_Sg_I_IsReadOnly;
+                    break;
+                case "Noun_Pl_N":
+                    Noun_Pl_N_IsReadOnly = !Noun_Pl_N_IsReadOnly;
+                    break;
+                case "Noun_Pl_A":
+                    Noun_Pl_A_IsReadOnly = !Noun_Pl_A_IsReadOnly;
+                    break;
+                case "Noun_Pl_G":
+                    Noun_Pl_G_IsReadOnly = !Noun_Pl_G_IsReadOnly;
+                    break;
+                case "Noun_Pl_P":
+                    Noun_Pl_P_IsReadOnly = !Noun_Pl_P_IsReadOnly;
+                    break;
+                case "Noun_Pl_D":
+                    Noun_Pl_D_IsReadOnly = !Noun_Pl_D_IsReadOnly;
+                    break;
+                case "Noun_Pl_I":
+                    Noun_Pl_I_IsReadOnly = !Noun_Pl_I_IsReadOnly;
+                    break;
+                default:
+                    MessageBox.Show("Internal error: unknown word form hash.");
+                    break;
+            }       // switch
         }
     }
 }
