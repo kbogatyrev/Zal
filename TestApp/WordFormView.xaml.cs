4f8e1b36c9761a14f84cfbea7bad3dc07c80945b
Konstantin <kbogatyrev@yahoo.com>
2019-02-16 08:33:57 -0800

    Initial revision.

---
+++ TestApp/WordFormView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ZalTestApp
+{
+    /// <summary>
+    /// Interaction logic for  WordFormView.xaml
+    /// </summary>
+    public partial class WordFormView : UserControl
+    {
+        public WordFormView()
+        {
+            InitializeComponent();
+        }
+    }
+}
