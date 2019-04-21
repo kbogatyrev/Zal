c900ca45003a464c380e4a2d843cd1087df7b664
Konstantin <kbogatyrev@yahoo.com>
2019-04-21 13:07:28 -0700

    Initial revision.

---
+++ TestApp/Skins.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ZalTestApp
+{
+    public partial class CustomResources : ResourceDictionary
+    {
+        public void ClickAddFormComment(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Тама!");
+        }
+    }
+}
