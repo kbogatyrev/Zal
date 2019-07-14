81e3f54cd010212d5da172b10c9cde611ee3d0a9
Konstantin <kbogatyrev@yahoo.com>
2019-07-14 16:23:37 -0700

    Text analysis -- first pass.

--- MainLib/Parser.h
+++ MainLib/Parser.h
@@ -10,6 +10,51 @@ class CSqlite;
 class CParsingTree;
 struct CWordForm;
 
+struct StWordParse
+{
+    int iNumber;
+    int iLineOffset;
+    int iLength;
+    int iPosInTactGroup;
+    long long llLineDbId;
+    long long llWordFormDbId;
+    long long llWordToWordFormId;
+    CWordForm* pWordForm;
+};
+
+//  CREATE TABLE tact_group(id INTEGER PRIMARY KEY ASC, line_id INTEGER, first_word_position INTEGER, num_of_words INTEGER, 
+//  source TEXT, transcription TEXT, stressed_syllable INTEGER, reverse_stressed_syllable INTEGER, FOREIGN KEY(line_id) REFERENCES lines_in_text(id));
+struct StTactGroup
+{
+    long long llLineId;
+    int iFirstWordNum;
+    int iNumOfWords;
+    int iStressPos;
+    int iReverseStressPos;
+    int iSecondaryStressPos;
+    CEString sSource;
+    CEString sTranscription;
+    vector<StWordParse> vecWords;
+
+    StTactGroup()
+    {
+        Reset();
+    }
+
+    void Reset()
+    {
+        llLineId = -1;
+        iFirstWordNum = -1;
+        iNumOfWords = 0;
+        iStressPos = -1;
+        iReverseStressPos = -1;
+        iSecondaryStressPos = -1;
+        sSource.Erase();
+        sTranscription.Erase();
+        vecWords.clear();
+    }
+};
+
 class CParser : public IParser
 {
 
@@ -44,8 +89,15 @@ private:
     ET_ReturnCode eFormLookup(const CEString&);
     ET_ReturnCode eRemoveFalsePositives();
     ET_ReturnCode eQueryDb(const CEString& sSelect, uint64_t& uiQueryHandle);
-    ET_ReturnCode eSaveWord(int iLine, int iWord, int iLineOffset, int iSegmentLength, long long& llWordDbKey);
-    ET_ReturnCode eSaveWordParse(long long llSegmentId, long long llWordFormId);
+    ET_ReturnCode eSaveLine(long long llTextId, int iLineNum, int iTextOffset, int iLength, int iNumOfWords, const CEString& sText, long long& llDbKey);
+    ET_ReturnCode eSaveWord(long long llLineDbId, int iLine, int iWord, int iLineOffset, int iSegmentLength, const CEString& sWord, long long& llWordDbKey);
+    ET_ReturnCode eSaveWordParse(long long llSegmentId, long long llWordFormId, long long& llWordToWordFormId);
+    ET_ReturnCode eSaveTactGroups(vector<StTactGroup>&);
+    ET_ReturnCode eClearTextData(long long llText);
+    bool bIsProclitic(CWordForm*);
+    bool bIsEnclitic(CWordForm*);
+    bool bArePhoneticallyIdentical(CWordForm* pWf1, CWordForm* pWf2);
+
     void HandleDbException(CException& ex);
 };
 
