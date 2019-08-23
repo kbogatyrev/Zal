ae1c72c67f4e4a56d07e3d0eca56816fcb48a0a1
Konstantin <kbogatyrev@yahoo.com>
2019-08-23 12:57:09 -0700

    Initial revision.

---
+++ TestApp/EnterTextParserDataDlg.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZalTestApp
+{
+    /// <summary>
+    /// Interaction logic for EnterTextParserDataDlgViewModel.xaml
+    /// </summary>
+    public partial class EnterTextParserDataDlg : Window
+    {
+        public EnterTextParserDataDlg(EnterTextParserDataDlgViewModel ctx)
+        {
+            InitializeComponent();
+            this.DataContext = ctx;
+        }
+    }
+
+/*
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            this.Close();
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+
+            var model = (EnterTextParserDataDlgViewModel)this.DataContext;
+
+            DialogResult = true;
+            this.Close();
+        }
+*/
+}
