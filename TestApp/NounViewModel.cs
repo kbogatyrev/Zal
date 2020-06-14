5fbe9d7997eed98f9929953ab07465bbb431a92a
Konstantin <kbogatyrev@yahoo.com>
2020-06-13 17:29:52 -0700

    Bug fix.

--- TestApp/NounViewModel.cs
+++ TestApp/NounViewModel.cs
@@ -589,7 +589,8 @@ namespace ZalTestApp
                         formsPerHash.iCurrentForm = 0;
                     }
 
-                    m_DictFormStatus[sHash] = formsPerHash;
+                    var sDisplayHash = sFormHashToDisplayHash(sHash);
+                    m_DictFormStatus[sDisplayHash] = formsPerHash;
                 }
             }
             catch (Exception ex)
