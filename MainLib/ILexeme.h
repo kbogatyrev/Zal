e4bf22e56a122c47cea723155bddc67d9147493a
Konstantin <kbogatyrev@yahoo.com>
2020-11-07 22:53:26 -0800

    Refactor spryazh. sm.

--- MainLib/ILexeme.h
+++ MainLib/ILexeme.h
@@ -15,6 +15,7 @@ namespace Hlib
     {
         virtual ~ILexeme() {};
         virtual __int64 llLexemeId() = 0;
+        virtual __int64 llHeadwordId() = 0;
         virtual void SetDictionary(CDictionary *) = 0;
         virtual const StLexemeProperties& stGetProperties() = 0;
         virtual StLexemeProperties& stGetPropertiesForWriteAccess() = 0;
