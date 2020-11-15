9b32e37f42247923251e3faf81c1327b66be83fc
Konstantin <kbogatyrev@yahoo.com>
2020-11-15 11:16:51 -0800

    Issue #332: Don't allow editing of spryazh. sm. verbs.

--- MainLib/ILexeme.h
+++ MainLib/ILexeme.h
@@ -172,6 +172,7 @@ namespace Hlib
         virtual void SetSecondPart(bool) = 0;
         virtual bool bHasPresPassParticiple() = 0;
         virtual void SetHasPresPassParticiple(bool) = 0;
+        virtual bool bSpryazhSm() = 0;
 
         virtual ET_ReturnCode eAddCommonDeviation(int iValue, bool bIsOptional) = 0;
         virtual bool bFindCommonDeviation(int iNum, bool& bIsOptional) = 0;
