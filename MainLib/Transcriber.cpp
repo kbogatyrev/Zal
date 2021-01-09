1a8a70473a2f43251f4368411cc185f9c7ef6009
Konstantin <kbogatyrev@yahoo.com>
2021-01-08 23:38:07 -0800

    Progress checkin.

--- MainLib/Transcriber.cpp
+++ MainLib/Transcriber.cpp
@@ -1,6 +1,7 @@
 ﻿#define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
 
 #include <memory>
+#include <cassert>
 
 #include "Transcriber.h"
 #include "SqliteWrapper.h"
@@ -26,222 +27,6 @@ CTranscriber::CTranscriber(CSqlite* pCSqlite) : m_pDb(pCSqlite)
         ERROR_LOG(L"No transcription rules.");
     }
 }
-
-ET_ReturnCode CTranscriber::eLoadTranscriptionRules()
-{
-    if (nullptr == m_pDb)
-    {
-        ERROR_LOG(L"Database not available.");
-        return H_ERROR_DB;
-    }
-
-    ET_ReturnCode eRet = H_NO_ERROR;
-
-    static const CEString sQuery
-    (L"SELECT ti.input_chars, tr.stress, tr.left_contexts, tr.right_contexts, tr.morpheme_type,  tr.subparadigm, \
-    tr.gramm_gender, tr.gramm_number, tr.gramm_case, tr.strength, tr.target FROM transcription_inputs \
-    AS ti INNER JOIN transcription_rules as tr ON ti.id = tr.input_id");
-
-    try
-    {
-        m_pDb->PrepareForSelect(sQuery);
-        while (m_pDb->bGetRow())
-        {
-            CEString sInputs;
-            m_pDb->GetData(0, sInputs);
-
-            PairInput pairInputs;
-            eRet = eFormatInputs(sInputs, pairInputs);
-
-            StRule rule;
-
-            // Stress (a string)
-            CEString sStressType;
-            m_pDb->GetData(1, sStressType);
-            if (!sStressType.bIsEmpty())
-            {
-                auto itStressType = m_mapStringToStressRelation.find(sStressType);
-                if (m_mapStringToStressRelation.end() == itStressType)
-                {
-                    ERROR_LOG(L"Stress context not recognized.");
-                    continue;
-                }
-                rule.m_vecStressContexts.push_back((*itStressType).second);
-            }
-
-            // Left contexts (an array of strings)
-            CEString sLeftContexts;
-            m_pDb->GetData(2, sLeftContexts);
-            if (!sLeftContexts.bIsEmpty())
-            {
-                vector<CEString> vecLeftContexts;
-                eRet = eSplitSource(sLeftContexts, vecLeftContexts);
-                if (eRet != H_NO_ERROR)
-                {
-                    ERROR_LOG(L"unable to split left context string.");
-                    continue;
-                }
-                for (auto sLeftContext : vecLeftContexts)
-                {
-                    auto itLeftContext = m_mapStringToContext.find(sLeftContext);
-                    if (m_mapStringToContext.end() == itLeftContext)
-                    {
-                        for (int iAt = 0; iAt < (int)sLeftContext.uiLength(); ++iAt)
-                        {
-                            auto chrAt = sLeftContext.chrGetAt(iAt);
-                            if (L'\0' == chrAt)
-                            {
-                                CEString sMsg(L"Unable to get character at pos. ");
-                                sMsg += CEString::sToString(iAt);
-                                ERROR_LOG(sMsg);
-                                continue;
-                            }
-// &&&&                            rule.m_vecLeftContexts.push_back
-                        }
-//                        ERROR_LOG(L"Left context not recognized.");
-//                        continue;
-                    }
-                    else
-                    {
-                        rule.m_vecLeftContexts.push_back((*itLeftContext).second);
-                    }
-                }
-            }       //  if (!sLeftContexts.bIsEmpty())
-
-            // Right contexts (an array of strings)
-            CEString sRightContexts;
-            m_pDb->GetData(3, sRightContexts);
-            if (!sRightContexts.bIsEmpty())
-            {
-                vector<CEString> vecRightContexts;
-                eRet = eSplitSource(sRightContexts, vecRightContexts);
-                if (eRet != H_NO_ERROR)
-                {
-                    ERROR_LOG(L"unable to split right context string.");
-                    continue;
-                }
-                for (auto sRightContext : vecRightContexts)
-                {
-                    auto itRightContext = m_mapStringToContext.find(sRightContext);
-                    if (m_mapStringToContext.end() == itRightContext)
-                    {
-                        ERROR_LOG(L"Right context not recognized.");
-                        continue;
-                    }
-                    rule.m_vecRightContexts.push_back((*itRightContext).second);
-                }
-            }
-
-            // Morphemic context: an array of strings
-            CEString sMorphemeType;
-            m_pDb->GetData(4, sMorphemeType);
-            if (!sMorphemeType.bIsEmpty())
-            {
-                vector<CEString> vecMorphemeTypes;
-                eRet = eSplitSource(sMorphemeType, vecMorphemeTypes);
-                if (eRet != H_NO_ERROR)
-                {
-                    ERROR_LOG(L"unable to split morphemic contexts string.");
-                    continue;
-                }
-                for (auto sMorphemeType : vecMorphemeTypes)
-                {
-                    auto itMorphemeType = m_mapStringToMorphemicContext.find(sMorphemeType);
-                    if (m_mapStringToMorphemicContext.end() == itMorphemeType)
-                    {
-                        ERROR_LOG(L"Morpheme not recognized.");
-                        continue;
-                    }
-                    rule.m_vecMorphemicContexts.push_back((*itMorphemeType).second);
-                }
-            }
-
-            // Subparadigm (a string)
-            CEString sSubparadigm;
-            m_pDb->GetData(5, sSubparadigm);
-            if (!sSubparadigm.bIsEmpty())
-            {
-                auto itSubparadigm = m_mapStringToSubparadigm.find(sSubparadigm);
-                if (m_mapStringToSubparadigm.end() == itSubparadigm)
-                {
-                    ERROR_LOG(L"Subparadigm not recognized.");
-                    continue;
-                }
-                rule.m_vecSubparadigms.push_back((*itSubparadigm).second);
-            }
-
-            // Gender (a string)
-            CEString sGender;
-            m_pDb->GetData(6, sGender);
-            if (!sGender.bIsEmpty())
-            {
-                auto itGender = m_mapStringToGender.find(sGender);
-                if (m_mapStringToGender.end() == itGender)
-                {
-                    ERROR_LOG(L"Gender not recognized.");
-                    continue;
-                }
-                rule.m_vecGenders.push_back((*itGender).second);
-            }
-
-            // Number (a string)
-            CEString sNumber;
-            m_pDb->GetData(7, sNumber);
-            if (!sNumber.bIsEmpty())
-            {
-                auto itNumber = m_mapStringToNumber.find(sNumber);
-                if (m_mapStringToNumber.end() == itNumber)
-                {
-                    ERROR_LOG(L"Number not recognized.");
-                    continue;
-                }
-                rule.m_vecNumbers.push_back((*itNumber).second);
-            }
-
-            // Case (a string)
-            CEString sCase;
-            m_pDb->GetData(8, sCase);
-            if (!sCase.bIsEmpty())
-            {
-                auto itCase = m_mapStringToCase.find(sCase);
-                if (m_mapStringToCase.end() == itCase)
-                {
-                    ERROR_LOG(L"Case not recognized.");
-                    continue;
-                }
-                rule.m_vecCases.push_back((*itCase).second);
-            }
-
-            // Strength (a string)
-            CEString sStrength;
-            m_pDb->GetData(9, sStrength);
-            if (!sStrength.bIsEmpty())
-            {
-                auto itStrength = m_mapStringToRuleStrength.find(sStrength);
-                if (m_mapStringToRuleStrength.end() == itStrength)
-                {
-                    ERROR_LOG(L"Strength not recognized.");
-                    continue;
-                }
-                rule.m_eStrength = (*itStrength).second;
-            }
-
-            // IsVariant (boolean value)
-            m_pDb->GetData(9, rule.m_bIsVariant);
-
-            // push back to the vector of rules for this input
-            m_mapRules[pairInputs].push_back(rule);
-        }
-    }
-    catch (CException & ex)
-    {
-        ERROR_LOG(ex.szGetDescription());
-        return H_EXCEPTION;
-    }
-
-    return H_NO_ERROR;
-
-}       //  eLoadTranscriptionRules()
 
 ET_ReturnCode CTranscriber::eFormatInputs(CEString& sSource, PairInput& pairParsedInputs)
 {
@@ -289,6 +74,58 @@ ET_ReturnCode CTranscriber::eSplitSource(CEString& sSource, vector<CEString>& ve
     return H_NO_ERROR;
 }
 
+ET_ReturnCode CTranscriber::eParseContexts(CEString& sSource, set<vector<PhonemicContextAtom>>& setTarget)
+{
+    assert(!sSource.bIsEmpty());
+
+    vector<CEString> vecContextStrings;
+    ET_ReturnCode eRet = eSplitSource(sSource, vecContextStrings);
+    if (eRet != H_NO_ERROR)
+    {
+        ERROR_LOG(L"unable to split context string.");
+        return H_ERROR_INVALID_ARG;
+    }
+
+    vector<PhonemicContextAtom> vecContexts;
+
+//    for (auto sContext : vecContextStrings)
+    for (unsigned int uiAt = 0; uiAt < vecContextStrings.size(); ++uiAt)
+    {
+        auto sContext = vecContextStrings[uiAt];
+
+        auto itContext = m_mapStringToContext.find(sContext);
+        PhonemicContextAtom context = ET_PhonemicContext::PHONEMIC_CONTEXT_UNDEFINED;
+        if (m_mapStringToContext.end() != itContext)
+        {
+            context = itContext->second;
+        }
+        else
+        {
+            if (CEString::bStringOverAlphabet(sContext, m_LowerCaseCyrillic))
+            {
+                context = sContext;
+            }
+            else
+            {
+                ERROR_LOG(L"Phonemic context characters not recognized.");
+                continue;
+            }
+        }
+
+        if (context != PhonemicContextAtom(ET_PhonemicContext::PHONEMIC_CONTEXT_UNDEFINED))
+        {
+            vecContexts.push_back(context);
+        }
+
+        if (!(uiAt < vecContextStrings.size()-1 && L"+" == vecContextStrings[uiAt + 1]))
+        {
+            setTarget.insert(vecContexts);
+            vecContexts.clear();
+        }
+    }
+
+    return H_NO_ERROR;
+}
 
 /*
 ET_ReturnCode CTranscriber::eFormatContexts(const CEString& sContext, EC_CONTEXT_TYPE eLeftOrRight)
@@ -307,3 +144,171 @@ ET_ReturnCode eSearchInSortedStrings(vector<CEString&> vecSortedStrings, const C
     return H_TRUE;
 }
 */
+
+ET_ReturnCode CTranscriber::eLoadTranscriptionRules()
+{
+    if (nullptr == m_pDb)
+    {
+        ERROR_LOG(L"Database not available.");
+        return H_ERROR_DB;
+    }
+
+    ET_ReturnCode eRet = H_NO_ERROR;
+
+    static const CEString sQuery
+    (L"SELECT ti.input_chars, tr.stress, tr.left_contexts, tr.right_contexts, tr.morpheme_type,  tr.subparadigm, \
+    tr.gramm_gender, tr.gramm_number, tr.gramm_case, tr.strength, tr.target FROM transcription_inputs \
+    AS ti INNER JOIN transcription_rules as tr ON ti.id = tr.input_id");
+
+    try
+    {
+        m_pDb->PrepareForSelect(sQuery);
+        while (m_pDb->bGetRow())
+        {
+            CEString sInputs;
+            m_pDb->GetData(0, sInputs);
+
+            PairInput pairInputs;
+            eRet = eFormatInputs(sInputs, pairInputs);
+
+            StRule rule;
+
+            // Stress (a string)
+            CEString sStressType;
+            m_pDb->GetData(1, sStressType);
+            if (!sStressType.bIsEmpty())
+            {
+                auto itStressType = m_mapStringToStressRelation.find(sStressType);
+                if (m_mapStringToStressRelation.end() == itStressType)
+                {
+                    ERROR_LOG(L"Stress context not recognized.");
+                    continue;
+                }
+                rule.m_setStressContexts.insert((*itStressType).second);
+            }
+
+            // Left contexts
+            CEString sLeftContexts;
+            m_pDb->GetData(2, sLeftContexts);
+            if (!sLeftContexts.bIsEmpty())
+            {
+                eRet = eParseContexts(sLeftContexts, rule.m_setLeftContexts);
+            }
+
+            // Right contexts
+            CEString sRightContexts;
+            m_pDb->GetData(3, sRightContexts);
+            if (!sRightContexts.bIsEmpty())
+            {
+                eRet = eParseContexts(sRightContexts, rule.m_setRightContexts);
+            }
+
+            // Morphemic context
+            CEString sMorphemeType;
+            m_pDb->GetData(4, sMorphemeType);
+            if (!sMorphemeType.bIsEmpty())
+            {
+                vector<CEString> vecMorphemeTypes;
+                eRet = eSplitSource(sMorphemeType, vecMorphemeTypes);
+                if (eRet != H_NO_ERROR)
+                {
+                    ERROR_LOG(L"unable to split morphemic contexts string.");
+                    continue;
+                }
+                for (auto sMorphemeType : vecMorphemeTypes)
+                {
+                    auto itMorphemeType = m_mapStringToMorphemicContext.find(sMorphemeType);
+                    if (m_mapStringToMorphemicContext.end() == itMorphemeType)
+                    {
+                        ERROR_LOG(L"Morpheme not recognized.");
+                        continue;
+                    }
+                    rule.m_setMorphemicContexts.insert((*itMorphemeType).second);
+                }
+            }
+
+            // Subparadigm (a string)
+            CEString sSubparadigm;
+            m_pDb->GetData(5, sSubparadigm);
+            if (!sSubparadigm.bIsEmpty())
+            {
+                auto itSubparadigm = m_mapStringToSubparadigm.find(sSubparadigm);
+                if (m_mapStringToSubparadigm.end() == itSubparadigm)
+                {
+                    ERROR_LOG(L"Subparadigm not recognized.");
+                    continue;
+                }
+                rule.m_setSubparadigms.insert((*itSubparadigm).second);
+            }
+
+            // Gender
+            CEString sGender;
+            m_pDb->GetData(6, sGender);
+            if (!sGender.bIsEmpty())
+            {
+                auto itGender = m_mapStringToGender.find(sGender);
+                if (m_mapStringToGender.end() == itGender)
+                {
+                    ERROR_LOG(L"Gender not recognized.");
+                    continue;
+                }
+                rule.m_setGenders.insert((*itGender).second);
+            }
+
+            // Number 
+            CEString sNumber;
+            m_pDb->GetData(7, sNumber);
+            if (!sNumber.bIsEmpty())
+            {
+                auto itNumber = m_mapStringToNumber.find(sNumber);
+                if (m_mapStringToNumber.end() == itNumber)
+                {
+                    ERROR_LOG(L"Number not recognized.");
+                    continue;
+                }
+                rule.m_setNumbers.insert((*itNumber).second);
+            }
+
+            // Case 
+            CEString sCase;
+            m_pDb->GetData(8, sCase);
+            if (!sCase.bIsEmpty())
+            {
+                auto itCase = m_mapStringToCase.find(sCase);
+                if (m_mapStringToCase.end() == itCase)
+                {
+                    ERROR_LOG(L"Case not recognized.");
+                    continue;
+                }
+                rule.m_setCases.insert((*itCase).second);
+            }
+
+            // Strength
+            CEString sStrength;
+            m_pDb->GetData(9, sStrength);
+            if (!sStrength.bIsEmpty())
+            {
+                auto itStrength = m_mapStringToRuleStrength.find(sStrength);
+                if (m_mapStringToRuleStrength.end() == itStrength)
+                {
+                    ERROR_LOG(L"Strength not recognized.");
+                    continue;
+                }
+                rule.m_eStrength = (*itStrength).second;
+            }
+
+            // IsVariant
+            m_pDb->GetData(9, rule.m_bIsVariant);
+
+            m_mapRules[pairInputs].push_back(rule);
+        }
+    }
+    catch (CException & ex)
+    {
+        ERROR_LOG(ex.szGetDescription());
+        return H_EXCEPTION;
+    }
+
+    return H_NO_ERROR;
+
+}       //  eLoadTranscriptionRules()
