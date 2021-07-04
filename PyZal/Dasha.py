fc97b4247dd34b417a751384d2516d365b390299
Konstantin <kbogatyrev@yahoo.com>
2021-07-04 10:21:38 -0700

    Change gram hash handling.

--- PyZal/Dasha.py
+++ PyZal/Dasha.py
@@ -2,16 +2,17 @@ import logging
 import re
 import sys
 import sqlite3
+import time
 
 class Data:
+    tact_group_id = -1
     source_with_stress = ''
-    position = -1
+    first_word_position = -1
     text = ''
     line_number = -1
     words_in_line = -1
     line_source = ''
-    garm_hash = ''
-    pos = ''
+    pos = []
 
 class Handler:
 
@@ -19,12 +20,9 @@ class Handler:
 
         self.db_connect = None
         self.db_cursor = None
-        self.words_query = 'SELECT tg.source, tg.gram_hash, tg.num_of_syllables, tg.stressed_syllable, tg.reverse_stressed_syllable, tg.id, lit.source, tg.first_word_position, lit.number_of_words, txt.name, lit.line_number \
-FROM tact_group AS tg INNER JOIN lines_in_text AS lit ON tg.line_id = lit.id INNER JOIN text AS txt ON txt.id = lit.text_id ORDER BY tg.source ASC'
-#        self.syll_count_query = 'SELECT tg.source, tg.gram_hash, lit.source, tg.first_word_position, txt.name, lit.line_number FROM tact_group AS tg INNER JOIN lines_in_text AS lit ON tg.line_id = lit.id \
-#INNER JOIN text AS txt ON txt.id = lit.text_id WHERE num_of_syllables = {} ORDER BY lit.source ASC'
-#        self.syll_count_query_with_stress = 'SELECT tg.source, tg.gram_hash, lit.source, tg.num_of_syllables, tg.stressed_syllable, tg.reverse_stressed_syllable, tg.first_word_position, txt.name, lit.line_number, lit.number_of_words FROM tact_group AS tg INNER JOIN \
-#lines_in_text AS lit ON tg.line_id = lit.id INNER JOIN text AS txt ON txt.id = lit.text_id WHERE num_of_syllables = {0} AND stressed_syllable = {1}'
+#                                     0       1               2                     3                   4                    5                        6                      7              8              9          10
+        self.words_query = 'SELECT tg.id, tg.source, tg.first_word_position, tg.num_of_words, tg.num_of_syllables, tg.stressed_syllable, tg.reverse_stressed_syllable, lit.source, lit.number_of_words, txt.name, lit.line_number \
+                            FROM tact_group AS tg INNER JOIN lines_in_text AS lit ON tg.line_id = lit.id INNER JOIN text AS txt ON txt.id = lit.text_id ORDER BY tg.source ASC' 
 
         self.data_dict = {}
 
@@ -49,22 +47,30 @@ FROM tact_group AS tg INNER JOIN lines_in_text AS lit ON tg.line_id = lit.id INN
             result_rows = self.db_cursor.fetchall()
             self.data_dict = {}
 
+            print ('Query returned {} rows'.format(len(result_rows)))
+
+            count = 0;
             for row in result_rows:
                 data = Data()
-                data.source_with_stress = row[0]
-                data.gram_hash = row[1]
-                data.num_of_syllables = row[2]
-                data.stressed_syllable = row[3] + 1
-                data.reverse_stressed_syllable = row[4] + 1
-                data.line_source = row[6]
-                data.position = row[7]
+                data.tact_group_id = row[0]
+                data.source_with_stress = row[1]
+                data.first_word_pos = row[2]
+                data.num_of_words = row[3]
+                data.num_of_syllables = row[4]
+                data.stressed_syllable = row[5] + 1
+                data.reverse_stressed_syllable = row[6] + 1
+                data.line_source = row[7]
                 data.text_name = row[9]
                 data.line_number = row[10]
-                data.is_last_word = '*' if row[7] + 1 == row[8] else ''
+                data.is_last_word = '*' if row[2] + 1 == row[8] else ''
 
-                data.pos = data.gram_hash.split('_')[0]
+                data.pos = self.get_gram_hashes(data)
 
-                self.data_dict[row[5]] = data
+                self.data_dict[data.tact_group_id] = data
+
+                count += 1
+                if count%1000 == 0:
+                    print(count)
 
             out_file = open(output_path, "w", encoding='utf16')
             out_data = None
@@ -79,13 +85,14 @@ FROM tact_group AS tg INNER JOIN lines_in_text AS lit ON tg.line_id = lit.id INN
                         cur_data.stressed_syllable == out_data.stressed_syllable and
                         cur_data.reverse_stressed_syllable == out_data.reverse_stressed_syllable and
                         cur_data.line_source == out_data.line_source and
-                        cur_data.position == out_data.position and
+                        cur_data.first_word_pos == out_data.first_word_pos and
                         cur_data.text_name == out_data.text_name and
                         cur_data.line_number == out_data.line_number and
                         cur_data.is_last_word == out_data.is_last_word):
                             if not(cur_data.pos in pos_tags):
                                 if len(cur_data.pos) > 0:
-                                    pos_tags.append(cur_data.pos)
+                                    pos_tags.extend(cur_data.pos)
+                                    pos_tags = list(set(pos_tags))
                     else:
                         pos_tags.sort()
                         pos_string = ''
@@ -95,32 +102,21 @@ FROM tact_group AS tg INNER JOIN lines_in_text AS lit ON tg.line_id = lit.id INN
                             pos_string += tag
 
                         out_file.write ('{0} | {1} | {2} | {3} | {4} | {5} | {6} | {7} | {8} \n'.format(out_data.source_with_stress, pos_string, out_data.num_of_syllables, out_data.stressed_syllable, \
-out_data.reverse_stressed_syllable, out_data.is_last_word, out_data.text_name, out_data.line_number, out_data.line_source))
+                            out_data.reverse_stressed_syllable, out_data.is_last_word, out_data.text_name, out_data.line_number, out_data.line_source))
                         out_data = cur_data
-                        pos_tags = [out_data.pos]
+                        pos_tags = list(set(out_data.pos))
 
         except Exception as e:
             print('Exception: {}'.format(e))
 
-
-    def get_words_by_num_of_syllables(self, num_of_syllables):
-        if not self.ready:
-            print('Error: Handler not initialized.')
-            return
-        try:
-            query = self.syll_count_query.format(num_of_syllables)
-            self.db_cursor.execute(query)
-            result_rows = self.db_cursor.fetchall()
-            for row in result_rows:
-                data = Data()
-                data.source_with_stress = row[0]
-                data.position = row[2]
-                data.text = row[3]
-                data.line_number = row[4]
-                data.words_in_line = row[5]
-
-        except Exception as e:
-            print('Exception: {}'.format(e))
+    def get_gram_hashes(self, data):
+        word_query = 'SELECT gram_hash FROM tact_group_to_gram_hash AS gh INNER JOIN tact_group AS tg ON gh.tact_group_id = tg.id WHERE tg.id = {};'.format(data.tact_group_id)
+        self.db_cursor.execute(word_query)
+        result_rows = self.db_cursor.fetchall()
+        gram_hashes = []
+        for row in result_rows:
+            gram_hashes.append(row[0].split('_')[0])
+        return list(set(gram_hashes))
 
     def sort_by_word(self):
         if not self.ready:
@@ -131,40 +127,10 @@ out_data.reverse_stressed_syllable, out_data.is_last_word, out_data.text_name, o
             value = self.data_dict[key]
             print('{0}| {1}| {2}| {3}'.format(key, value.text, value.line_number, value.position))
 
-    def get_words_by_stress_pos(self):
-        if not self.ready:
-            print('Error: Handler not initialized.')
-            return
-#        tg.source, lit.source, tg.first_word_position, txt.name, lit.line_number, tg.reverse_stressed_syllable
-        try:
-            for num_of_syllables in range (3, 6):
-                for stress_pos in range (0, num_of_syllables):
-                    query = self.syll_count_query_with_stress.format(num_of_syllables, stress_pos)
-                    self.db_cursor.execute(query)
-                    result_rows = self.db_cursor.fetchall()
-                    self.data_dict = {}
-                    for row in result_rows:
-                        data = Data()
-                        data.source_with_stress = row[0]  # tg.source
-                        data.position = row[2]            # position in line
-                        data.text = row[3]                # source
-                        data.line_number = row[4]
-                        data.words_in_line = row[5]
-
-                        if data.words_in_line - 1 == data.position:
-                            self.data_dict[row[0]] = data
-
-                    self.sort_by_word()
-
-        except Exception as e:
-            print('Exception: {}'.format(e))
-
 if __name__== "__main__":
-    db_path = '../ZalData/ZalData_analytics_test.db3'
-    output_path = '../ZalData/Dasha_test.csv'
+    db_path = '../ZalData/ZalData_Master_Pasternak_07_03_2021.db3'
+    output_path = '../ZalData/Pasternak_07_03.csv'
     h = Handler(db_path)
-#    h.get_words_by_stress_pos()
-#    h.get_words_by_num_of_syllables(3)
     h.get_words()
 
     k = 0
